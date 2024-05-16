d155d119d28421b52aa5f960c00e039a8d3e740b
Pieryvdl <pieryvanderlinde@gmail.com>
2024-05-16 18:44:21 +0200

    testing changes

--- gameonconnect/lib/main.dart
+++ gameonconnect/lib/main.dart
@@ -39,7 +39,7 @@ class MyApp extends StatelessWidget {
         //
         // This works for code too, not just values: Most code changes can be
         // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
         useMaterial3: true,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
