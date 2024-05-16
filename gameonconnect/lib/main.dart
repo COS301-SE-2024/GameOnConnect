9d4e52207288046618dafeddc919dfbd3bfed66c
Pieryvdl <pieryvanderlinde@gmail.com>
2024-05-16 18:39:27 +0200

    added .env as an asset

--- gameonconnect/lib/main.dart
+++ gameonconnect/lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart'; 
-import 'package:flutter_dotenv/flutter_dotenv.dart' as dotenv;
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
