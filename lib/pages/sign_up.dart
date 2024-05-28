19996ad208d08747213d7c84152a0cf22f50ce21
franco-dreyer <u22520016@tuks.co.za>
2024-05-29 01:30:26 +0200

    Added integration with database to Sign Up
    I added code to integrate with the database for the Sign Up page so that it actually successfully creates a user.  I also added some validation checks to each input field to make sure the correct data is stored in the database.  I am unsure about adding the username to the database, though.

--- lib/pages/sign_up.dart
+++ lib/pages/sign_up.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 const double _textFieldWidth = 300;
 const InputDecoration _inputDecoration = InputDecoration(
@@ -10,6 +11,23 @@ class SignUp extends StatelessWidget {
   final _emailController = TextEditingController();
   final _passwordController = TextEditingController();
   final _confirmPasswordController = TextEditingController();
+  final _formKey = GlobalKey<FormState>();
+
+  SignUp({super.key});
+
+  Future signUp() async {
+    await FirebaseAuth.instance.createUserWithEmailAndPassword(
+      email: _emailController.text.trim(),
+      password: _passwordController.text.trim(),
+    );
+  }
+
+  void dispose() {
+    _emailController.dispose();
+    _passwordController.dispose();
+    _usernameController.dispose();
+    _confirmPasswordController.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -29,16 +47,23 @@ class SignUp extends StatelessWidget {
                   )),
             ),
             Form(
+              key: _formKey,
               child: Column(
                 children: [
                   Padding(
                     padding: const EdgeInsets.all(8.0),
                     child: SizedBox(
                       width: _textFieldWidth,
-                      child: TextField(
+                      child: TextFormField(
                         controller: _usernameController,
                         decoration:
                             _inputDecoration.copyWith(labelText: 'Username'),
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Please enter a username';
+                          }
+                          return null;
+                        },
                       ),
                     ),
                   ),
@@ -46,10 +71,21 @@ class SignUp extends StatelessWidget {
                     padding: const EdgeInsets.all(8.0),
                     child: SizedBox(
                       width: _textFieldWidth,
-                      child: TextField(
+                      child: TextFormField(
                         controller: _emailController,
                         decoration:
                             _inputDecoration.copyWith(labelText: 'Email'),
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Please enter an email address';
+                          }
+                          if (!RegExp(
+                                  r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+")
+                              .hasMatch(value)) {
+                            return 'Please enter a valid email address';
+                          }
+                          return null;
+                        },
                       ),
                     ),
                   ),
@@ -57,11 +93,22 @@ class SignUp extends StatelessWidget {
                     padding: const EdgeInsets.all(8.0),
                     child: SizedBox(
                       width: _textFieldWidth,
-                      child: TextField(
+                      child: TextFormField(
                         controller: _passwordController,
                         obscureText: true,
                         decoration:
                             _inputDecoration.copyWith(labelText: 'Password'),
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Please enter a password';
+                          }
+                          if (!RegExp(
+                                  r'^(?=.*[A-Z])(?=.*[!@#$%^&*(),.?":{}|<>]).{8,}$')
+                              .hasMatch(value)) {
+                            return 'Password must contain an uppercase letter, a symbol and at least 8 characters';
+                          }
+                          return null;
+                        },
                       ),
                     ),
                   ),
@@ -69,11 +116,20 @@ class SignUp extends StatelessWidget {
                     padding: const EdgeInsets.all(8.0),
                     child: SizedBox(
                       width: _textFieldWidth,
-                      child: TextField(
+                      child: TextFormField(
                         controller: _confirmPasswordController,
                         obscureText: true,
                         decoration: _inputDecoration.copyWith(
                             labelText: 'Confirm Password'),
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Please confirm your password';
+                          }
+                          if (value != _passwordController.text) {
+                            return 'Passwords do not match';
+                          }
+                          return null;
+                        },
                       ),
                     ),
                   ),
@@ -85,9 +141,13 @@ class SignUp extends StatelessWidget {
               width: _textFieldWidth,
               height: 50,
               child: FilledButton(
+                style: ButtonStyle(
+                    backgroundColor: WidgetStateProperty.all(Colors.purple)),
                 onPressed: () {
                   // Validate and process the form.
-                  print("signup");
+                  if (_formKey.currentState!.validate()) {
+                    signUp();
+                  }
                 },
                 child: const Text(
                   "Sign Up",
