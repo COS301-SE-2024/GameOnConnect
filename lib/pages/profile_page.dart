42727773410ba26e6036bc59e047630784f98f10
LeratoKgomo <162160922+LeratoKgomo@users.noreply.github.com>
2024-05-28 14:04:43 +0200

    Created profile class

---
+++ lib/pages/profile_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Profile extends StatelessWidget {
+  const Profile({super.key});
+
+   Widget build(BuildContext context)
+   {
+    return Scaffold();
+   }
+}
