44bba40883746f55422f92f090b40cf795240d76
Shannon <131780205+shan-226@users.noreply.github.com>
2024-05-30 12:06:33 +0200

    ✅ Test - Verify if form fields are present

--- test/edit_profile_page_test.dart
+++ test/edit_profile_page_test.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:gameonconnect/pages/edit_profile_page.dart'; // Adjust to your package name
+import 'package:gameonconnect/pages/edit_profile_page.dart'; 
 
 void main() {
   testWidgets('EditProfilePage builds correctly', (WidgetTester tester) async {
@@ -12,5 +12,17 @@ void main() {
     expect(find.byType(CircleAvatar), findsOneWidget);
     // Verify if the EditProfileForm is present
     expect(find.byType(EditProfileForm), findsOneWidget);
+  });
+
+  testWidgets('Form fields are present and editable', (WidgetTester tester) async {
+    await tester.pumpWidget(MaterialApp(home: EditProfilePage()));
+
+    // Verify if the form fields are present
+    expect(find.text('Username:'), findsOneWidget);
+    expect(find.text('First Name:'), findsOneWidget);
+    expect(find.text('Last Name:'), findsOneWidget);
+    expect(find.text('Bio:'), findsOneWidget);
+
+    
   });
 }
