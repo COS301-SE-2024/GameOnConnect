19181d90900c5f8239c3b274c9dd8d4ce19ccd61
LeratoKgomo <162160922+LeratoKgomo@users.noreply.github.com>
2024-05-30 11:48:07 +0200

    Added the appBar

--- lib/pages/customize_page.dart
+++ lib/pages/customize_page.dart
@@ -12,7 +12,24 @@ class CustomizeProfilePageObject extends State<CustomizeProfilePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-     
+      appBar: AppBar(
+         leading:IconButton(
+            icon: const Icon(Icons.keyboard_backspace),
+           onPressed: () {
+            Navigator.of(context).pop();
+          },
+        ),
+        title: CircleAvatar(
+          radius: 25.0, // Doubled the radius
+          backgroundColor: Colors.white,
+          child: Container(
+            decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              border: Border.all(color: Colors.black, width: 2.0),
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
