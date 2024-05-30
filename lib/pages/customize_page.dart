ef60fc6113e96b4fb9d24a17c264df37ade4a7ec
LeratoKgomo <162160922+LeratoKgomo@users.noreply.github.com>
2024-05-30 11:39:02 +0200

    Created customize profile page

---
+++ lib/pages/customize_page.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class CustomizeProfilePage extends StatefulWidget {
+  @override
+  CustomizeProfilePageObject createState() => CustomizeProfilePageObject();
+}
+
+class CustomizeProfilePageObject extends State<CustomizeProfilePage> {
+
+  bool isDarkMode = false;
+  
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+     
+    );
+  }
+}
