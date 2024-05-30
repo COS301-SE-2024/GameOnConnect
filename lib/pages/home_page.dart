385bbbbb966d6660ca56f239f152ee60f03a683b
LeratoKgomo <162160922+LeratoKgomo@users.noreply.github.com>
2024-05-30 10:55:38 +0200

    removed unused import

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,7 +1,6 @@
 // ignore_for_file: prefer_const_constructors
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:gameonconnect/pages/profile_page.dart';
 
 class HomePage extends StatefulWidget {
   // ignore: use_super_parameters
