39bc19ccee1253bfd0e0486e075ddc82de138765
franco-dreyer <u22520016@tuks.co.za>
2024-05-30 11:27:07 +0200

    Updated the Game Library page to work with API
    I updated the Game Library page to fetch game data from the RAWG API.

--- lib/pages/game_library_page.dart
+++ lib/pages/game_library_page.dart
@@ -1,7 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
-class GameLibrary extends StatelessWidget {
+class GameLibrary extends StatefulWidget {
   const GameLibrary({super.key});
+
+  @override
+  State<GameLibrary> createState() => _GameLibraryState();
+}
+
+class _GameLibraryState extends State<GameLibrary> {
+  List<Game> _games = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _loadGames();
+  }
+
+  Future<void> _loadGames() async {
+    final response = await http.get(Uri.parse(
+        'https://api.rawg.io/api/games?key=b8d81a8e79074f1eb5c9961a9ffacee6&page_size=50'));
+
+    if (response.statusCode == 200) {
+      final jsonData = jsonDecode(response.body);
+      final games =
+          jsonData['results'].map<Game>((gameJson) => Game.fromJson(gameJson)).toList();
+      setState(() {
+        _games = games;
+      });
+    } else {
+      throw Exception('Failed to load games');
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -14,28 +45,34 @@ class GameLibrary extends StatelessWidget {
           ),
         ),
       ),
-      body: ListView(
-        children: const [
-          Card(
+      body: ListView.builder(
+        itemCount: _games.length,
+        itemBuilder: (context, index) {
+          final game = _games[index];
+          return Card(
             child: ListTile(
-              title: Text('Game 1'),
-              subtitle: Text('This is a description of Game 1'),
+              title: Text(game.name),
+              subtitle: Text(game.released as String),
             ),
-          ),
-          Card(
-            child: ListTile(
-              title: Text('Game 2'),
-              subtitle: Text('This is a description of Game 2'),
-            ),
-          ),
-          Card(
-            child: ListTile(
-              title: Text('Game 3'),
-              subtitle: Text('This is a description of Game 3'),
-            ),
-          ),
-        ],
+          );
+        },
       ),
+    );
+  }
+}
+
+class Game {
+  final int id;
+  final String name;
+  final String released;
+
+  Game({required this.id, required this.name, required this.released});
+
+  factory Game.fromJson(Map<String, dynamic> json) {
+    return Game(
+      id: json['id'],
+      name: json['name'],
+      released: json['released'],
     );
   }
 }
