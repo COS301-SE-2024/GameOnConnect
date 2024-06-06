1cea233b3a59c39e04eb7d092499383cea479efd
Pieryvdl <pieryvanderlinde@gmail.com>
2024-06-06 08:14:49 +0200

    added logo to signup page

--- lib/pages/sign_up.dart
+++ lib/pages/sign_up.dart
@@ -3,36 +3,33 @@ import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
-
 const double _textFieldWidth = 300;
 const InputDecoration _inputDecoration = InputDecoration(
   border: OutlineInputBorder(),
 );
 int? _nextNum;
 Future<void> getNextNumber() async {
-
   FirebaseFirestore db = FirebaseFirestore.instance;
   CollectionReference profileRef = db.collection("next_digit");
   DocumentSnapshot qs = await profileRef.doc("current_max_digit").get();
 
-  if(qs.exists) {
-    Map<String,dynamic> d = qs.data() as Map<String,dynamic>;
+  if (qs.exists) {
+    Map<String, dynamic> d = qs.data() as Map<String, dynamic>;
     _nextNum = d['digit'];
   }
   int da = _nextNum ?? 0;
-  db.collection("next_digit").doc("current_max_digit").update({"digit":(da+1)});
-
-
+  db
+      .collection("next_digit")
+      .doc("current_max_digit")
+      .update({"digit": (da + 1)});
 }
 
-Future<void> createDefaultProfile() async
-{
-  try{
-
+Future<void> createDefaultProfile() async {
+  try {
     FirebaseFirestore db = FirebaseFirestore.instance;
     final FirebaseAuth auth = FirebaseAuth.instance;
     final currentUser = auth.currentUser;
-    if(currentUser != null) {
+    if (currentUser != null) {
       final defaultData = <String, dynamic>{
         "name": "",
         "surname": "",
@@ -42,23 +39,20 @@ Future<void> createDefaultProfile() async
         "profile_picture": "gameonconnect-cf66d.appspot.com/default_image.jpg",
         "social_interests_tags": [],
         "theme": "light",
-        "userID":  currentUser.uid,
+        "userID": currentUser.uid,
         // change this to dynamically add the user's id
         "username": {"profile_name": _username, "unique_num": _nextNum},
         "visibility": true
       };
 
-      db.collection("profile_data")
-          .doc(currentUser.uid)
-          .set(defaultData);
+      db.collection("profile_data").doc(currentUser.uid).set(defaultData);
     }
-
-  }catch(e)
-  { // do nothing
+  } catch (e) {
+    // do nothing
   }
-
 }
-String? _username ;
+
+String? _username;
 
 class SignUp extends StatelessWidget {
   final _usernameController = TextEditingController();
@@ -84,6 +78,7 @@ class SignUp extends StatelessWidget {
     _usernameController.dispose();
     _confirmPasswordController.dispose();
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -93,7 +88,13 @@ class SignUp extends StatelessWidget {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              const Icon(Icons.logo_dev, size: 100),
+              Image.asset(
+                Theme.of(context).brightness == Brightness.dark
+                    ? 'assets/icons/Logo_dark.png'
+                    : 'assets/icons/Logo_light.png',
+                width: 100,
+                height: 100,
+              ),
               const Padding(
                 padding: EdgeInsets.all(20),
                 child: Text("Sign Up",
@@ -167,7 +168,8 @@ class SignUp extends StatelessWidget {
                             if (!RegExp(r'^.*[A-Z].*$').hasMatch(value)) {
                               return 'Password must contain an uppercase letter';
                             }
-                            if (!RegExp(r'^.*[!@#$%^&*(),.?":{}|<>].*$').hasMatch(value)) {
+                            if (!RegExp(r'^.*[!@#$%^&*(),.?":{}|<>].*$')
+                                .hasMatch(value)) {
                               return 'Password must contain a symbol';
                             }
                             return null;
@@ -213,7 +215,6 @@ class SignUp extends StatelessWidget {
                       _username = _usernameController.text;
                       signUp();
                       Navigator.of(context).pop();
-
                     }
                   },
                   child: const Text(
