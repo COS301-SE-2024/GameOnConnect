2efa1ad4a8f04e5acc6b5f91f889a2c36323b175
Shannon <131780205+shan-226@users.noreply.github.com>
2024-06-10 18:28:59 +0200

    🚧 testing

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,6 +1,6 @@
 // ignore_for_file: prefer_const_constructors
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class HomePage extends StatefulWidget {
   // ignore: use_super_parameters
@@ -12,6 +12,21 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  int _selectedIndex = 0;
+  static const List<Widget> _pages = <Widget>[
+    Text('Home Page'),
+    Text('Games Page'),
+    Text('Customize Page'),
+    Text('Calendar Page'),
+    Text('Profile Page'),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,55 +35,7 @@ class _HomePageState extends State<HomePage> {
         title: Text(widget.title),
       ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            MaterialButton(
-              onPressed: () {
-                FirebaseAuth.instance.signOut();
-                FirebaseAuth.instance.signOut();
-              },
-              color: Colors.grey[600],
-              child: Text('Sign Out'),
-            ),
-            /* MaterialButton(
-              onPressed: () {
-                Navigator.pushNamed(context, '/edit-profile');
-              },
-              color: Colors.grey[600],
-              child: Text('Edit Profile'),
-            ),
-            MaterialButton(
-              onPressed: () {
-                Navigator.pushNamed(context, '/customize');
-              },
-              color: Colors.grey[600],
-              child: Text('Customize profile '),
-            ),
-            */
-             MaterialButton(
-              onPressed: () {
-                Navigator.pushNamed(context, '/sign_up');
-              },
-              color: Colors.grey[600],
-              child: Text('sign up page '),
-            ),
-            MaterialButton(
-              onPressed: () {
-                Navigator.pushNamed(context, '/profile');
-              },
-              color: Colors.grey[600],
-              child: Text('Profile Page '),
-            ),
-            MaterialButton(
-              onPressed: () {
-                Navigator.pushNamed(context, '/game_library');
-              },
-              color: Colors.grey[600],
-              child: Text('Game Library Page '),
-            ),
-          ],
-        ),
+        child: _pages.elementAt(_selectedIndex),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
@@ -81,37 +48,44 @@ class _HomePageState extends State<HomePage> {
       bottomNavigationBar: BottomAppBar(
         shape: CircularNotchedRectangle(),
         notchMargin: 8.0,
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children: <Widget>[
-            IconButton(
-              icon: Icon(Icons.home_filled),
-              onPressed: () {
-                // _onItemTapped(0);
-                // Navigator.pushNamed(context, '/home');
-              },
-            ),
-            IconButton(
-              icon: Icon(Icons.search),
-              onPressed: () {
-                // _onItemTapped(1);
-                // add link
-              },
-            ),
-            SizedBox(width: 48.0), // The dummy child
-            IconButton(
-              icon: Icon(Icons.calendar_today),
-              onPressed: () {
-                // _onItemTapped(2);
-                // add link
-              },
-            ),
-            IconButton(
-              icon: Icon(Icons.person),
-              onPressed: () {
-                // _onItemTapped(3);
-                Navigator.pushNamed(context, '/profile');
-              },
+        child: Container(
+          height: 60.0,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              _buildNavItem(Icons.home_filled, 0),
+              _buildNavItem(Icons.sports_esports, 1),
+              SizedBox(width: 48.0), // Space for the floating action button
+              _buildNavItem(Icons.calendar_today, 2),
+              _buildNavItem(Icons.person, 3),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildNavItem(IconData icon, int index) {
+    return GestureDetector(
+      onTap: () => _onItemTapped(index),
+      child: Container(
+        width: MediaQuery.of(context).size.width / 5,
+        height: 60.0,
+        child: Stack(
+          alignment: Alignment.center,
+          children: [
+            if (_selectedIndex == index)
+              Container(
+                width: 40.0,
+                height: 40.0,
+                decoration: BoxDecoration(
+                  color: Colors.green,
+                  shape: BoxShape.circle,
+                ),
+              ),
+            Icon(
+              icon,
+              color: _selectedIndex == index ? Colors.white : Colors.grey,
             ),
           ],
         ),
