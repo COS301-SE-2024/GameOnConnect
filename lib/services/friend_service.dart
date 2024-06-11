1a98de606eadb747f0139fa145c9617d18a464eb
Pieryvdl <pieryvanderlinde@gmail.com>
2024-06-11 15:33:07 +0200

    added to get the friends from firebase

--- lib/services/friend_service.dart
+++ lib/services/friend_service.dart
@@ -1,5 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 
 class FriendServices {
   //get an instance from FireStore Database
@@ -31,6 +32,46 @@ class FriendServices {
 
     } catch (e) {
       return []; //return an empty array
+    }
+  }
+
+  Future<Map<String, dynamic>?> fetchFriendProfileData(String userId) async {
+    try {
+      FirebaseFirestore db = FirebaseFirestore.instance;
+
+      DocumentSnapshot doc =
+          await db.collection("profile_data").doc(userId).get();
+
+      if (doc.exists) {
+        Map<String, dynamic> data = doc.data() as Map<String, dynamic>;
+        Map<String, dynamic> userInfo =
+            data['username'] as Map<String, dynamic>;
+        String profileName = data['name'] ?? 'Profile name';
+        String username = userInfo['profile_name'] ?? 'username';
+        String profilePicture = data['profile_picture'] ?? '';
+
+        String profilePictureUrl = '';
+
+        if (profilePicture.isNotEmpty) {
+          try {
+            Reference storageRef =
+                FirebaseStorage.instance.refFromURL(profilePicture);
+            profilePictureUrl = await storageRef.getDownloadURL();
+          } catch (e) {
+            return null;
+          }
+        }
+
+        return {
+          'profileName': profileName,
+          'username': username,
+          'profilePicture': profilePictureUrl,
+        };
+      } else {
+        return null;
+      }
+    } catch (e) {
+      return null;
     }
   }
 }
