15e8d75f7026106a722f95812c84823e425e369b
Pieryvdl <pieryvanderlinde@gmail.com>
2024-06-11 11:13:06 +0200

    moved the profile service code

--- lib/pages/profile_page.dart
+++ lib/pages/profile_page.dart
@@ -1,72 +1,18 @@
 // ignore_for_file: prefer_const_constructors
 import 'package:flutter/material.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_storage/firebase_storage.dart';
 import 'package:gameonconnect/services/friend_service.dart';
+import 'package:gameonconnect/services/profile_service.dart';
 
 class Profile extends StatelessWidget {
-  const Profile({super.key});
+  Profile({super.key});
 
-  //Function to query FireStore
-  Future<Map<String, dynamic>?> fetchProfileData() async {
-    try {
-      FirebaseFirestore db = FirebaseFirestore.instance;
-      final FirebaseAuth auth = FirebaseAuth.instance;
-      final currentUser = auth.currentUser;
-
-      if (currentUser != null) {
-        DocumentSnapshot doc =
-            await db.collection("profile_data").doc(currentUser.uid).get();
-
-        if (doc.exists) {
-          Map<String, dynamic> data = doc.data() as Map<String, dynamic>;
-          Map<String, dynamic> userInfo =
-              data['username'] as Map<String, dynamic>;
-          String profileName = data['name'] ?? 'Profile name';
-          String username = userInfo['profile_name'] ?? 'username';
-          String profilePicture = data['profile_picture'] ?? '';
-          String profileBanner = data['banner'];
-
-          String profilePictureUrl = '';
-          String bannerUrl = '';
-
-          if (profilePicture.isNotEmpty) {
-            try {
-              // Use refFromURL for a full URL
-              Reference storageRef =
-                  FirebaseStorage.instance.refFromURL(profilePicture);
-              profilePictureUrl = await storageRef.getDownloadURL();
-
-              Reference storage2 =
-                  FirebaseStorage.instance.refFromURL(profileBanner);
-              bannerUrl = await storage2.getDownloadURL();
-            } catch (e) {
-              return null;
-            }
-          }
-
-          return {
-            'profileName': profileName,
-            'username': username,
-            'profilePicture': profilePictureUrl,
-            'profileBanner': bannerUrl
-          };
-        } else {
-          return null;
-        }
-      } else {
-        return null;
-      }
-    } catch (e) {
-      return null;
-    }
-  }
-
+  // Create an instance of ProfileService
+  final profileService = ProfileService();
+  
   @override
   Widget build(BuildContext context) {
     return FutureBuilder<Map<String, dynamic>?>(
-      future: fetchProfileData(),
+      future: profileService.fetchProfileData(),
       builder: (context, snapshot) {
         if (snapshot.connectionState == ConnectionState.waiting) {
           return Scaffold(
