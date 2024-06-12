a2a8f6e954667d54f24503a38ad4cceb40acd220
Monika Theiss <u21434558@tuks.co.za>
2024-06-12 21:27:25 +0200

    Added drop down content on the profile management, so the user can choose what they need help with

--- lib/pages/profile_management_page.dart
+++ lib/pages/profile_management_page.dart
@@ -1,46 +1,66 @@
-
 import 'package:flutter/material.dart';
 
 class Item {
-  Item({
-    required this.headerText,
-    required this.expandedText,
-    this.expanded = false
-  });
+  Item(
+      {required this.headerText,
+      required this.expandedText,
+      this.expanded = false});
   String headerText;
   String expandedText;
   bool expanded;
 }
+
 List<Item> getItems() {
   return [
-    Item(headerText: 'Change my username', expandedText: 'To change your '
-        'username, navigate to the settings on your profile page,'
-        'and then select the Edit Profile option.'),
-    Item(headerText: 'Change my first name', expandedText: 'To change your '
-        'first name, navigate to the settings on your profile page,'
-        'and then select the Edit Profile option.')
+    Item(
+        headerText: 'Change my username',
+        expandedText: 'To change your '
+            'username, navigate to the settings on your profile page,'
+            'and then select the Edit Profile option.'),
+    Item(
+        headerText: 'Change my first name',
+        expandedText: 'To change your '
+            'first name, navigate to the settings on your profile page,'
+            'and then select the Edit Profile option.')
   ];
 }
 
-
-class ProfileManagement extends StatelessWidget{
+class ProfileManagement extends StatelessWidget {
   const ProfileManagement({super.key});
   //List<Item> items = getItems();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        leading: IconButton(
-          icon: const Icon(Icons.keyboard_backspace),
-          onPressed: () {
-            Navigator.pop(context);
-          },
+        appBar: AppBar(
+          leading: IconButton(
+            icon: const Icon(Icons.keyboard_backspace),
+            onPressed: () {
+              Navigator.pop(context);
+            },
+          ),
         ),
-      ),
-      body: const Center(
+        body: const Column(children: [
+          ExpansionTile(
+            title: Text('Change my username'),
+            children: <Widget>[
+              ListTile(
+                title: Text('To change your username, navigate to the '
+                    'settings on your profile page,and then select the'
+                    ' Edit Profile option.'),
+              ),
 
-      ),
-    );
+            ],
+          ),
+          ExpansionTile(
+            title: Text('Change my first name'),
+            children: [
+              ListTile(
+                title: Text('To change your first name, navigate'
+                    ' to the settings on your profile page,and then'
+                    ' select the Edit Profile option.'),
+              )
+            ],
+          )
+        ]));
   }
-
 }
