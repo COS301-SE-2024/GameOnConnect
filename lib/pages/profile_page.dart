56b1f68448671048fddf2428535bacc772a3211b
Pieryvdl <pieryvanderlinde@gmail.com>
2024-06-13 10:01:33 +0200

    fixed the imports giving errors.

--- lib/pages/profile_page.dart
+++ lib/pages/profile_page.dart
@@ -1,8 +1,6 @@
 // ignore_for_file: prefer_const_constructors
 import 'package:flutter/material.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_storage/firebase_storage.dart';
 import 'package:gameonconnect/pages/login_page.dart';
 import 'package:gameonconnect/services/friend_service.dart';
 import 'package:gameonconnect/services/profile_service.dart';
