90ba196398e4bbd4403d9064b459d5fa63273046
Shannon <131780205+shan-226@users.noreply.github.com>
2024-06-16 21:27:01 +0200

    ➕ blank filler page

---
+++ lib/pages/currently_playing_page.dart
@@ -0,0 +1,16 @@
+// ignore_for_file: prefer_const_constructors
+import 'package:flutter/material.dart';
+
+class CurrentlyPlayingPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Currently Playing'),
+      ),
+      body: Center(
+        child: Text('Currently Playing Page'),
+      ),
+    );
+  }
+}
