ed7a5d82cf97d99deb212c1b737b3fe5858fb60b
Shannon <131780205+shan-226@users.noreply.github.com>
2024-06-16 21:26:41 +0200

    ➕ blank filler page

---
+++ lib/pages/events_page.dart
@@ -0,0 +1,16 @@
+// ignore_for_file: prefer_const_constructors
+import 'package:flutter/material.dart';
+
+class EventsPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Events'),
+      ),
+      body: Center(
+        child: Text('Events Page'),
+      ),
+    );
+  }
+}
