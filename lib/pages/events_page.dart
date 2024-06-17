4481eec703514e8cffd534f1819af89eb73c9608
Shannon <131780205+shan-226@users.noreply.github.com>
2024-06-17 18:56:35 +0200

    🐛 flutter analyze fixes

--- lib/pages/events_page.dart
+++ lib/pages/events_page.dart
@@ -2,6 +2,8 @@
 import 'package:flutter/material.dart';
 
 class EventsPage extends StatelessWidget {
+  const EventsPage({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
