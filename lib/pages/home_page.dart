faebf0c25e3b4758a91e12e599dcea2f3c6614ca
Monika Theiss <u21434558@tuks.co.za>
2024-06-21 17:00:32 +0200

    Testing wishlist services functions.

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,7 +1,7 @@
 // ignore_for_file: prefer_const_constructors
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-
+import 'package:gameonconnect/services/wishlist_service.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 class HomePage extends StatefulWidget {
   // ignore: use_super_parameters
@@ -18,10 +18,60 @@ class _HomePageState extends State<HomePage> {
   @override
   void initState() {
     super.initState();
+
     //createDefaultProfile();
-    databaseAccess();
+    //databaseAccess();
+    wishlistFunctions();
     //editProfile();
 
+  }
+  Future <void> wishlistFunctions() async{
+    getWL();
+    //await addToWL('10');
+    await removeFromWL('10');
+    getWL();
+  }
+
+  Future<void> getWL() async{
+    try{
+      final wishlist = Wishlist();
+      List<String> w = await wishlist.getWishlist();
+      print(w);
+    setState((){
+      _counter = "wishlist: $w";
+    });
+  }catch (e)
+    {
+      setState((){
+        _counter = 'Error: $e';
+    });
+    };
+  }
+
+  Future addToWL(String i) async {
+    try
+        {
+          final w = Wishlist();
+          await w.addToWishlist(i);
+
+        }catch (e)
+    {
+      setState((){
+        _counter = 'error in add: $e';
+      });
+    }
+  }
+
+  Future<void> removeFromWL(String i) async{
+    try{
+      final w = Wishlist();
+      await w.removeFromWishlist(i);
+    }catch(e)
+    {
+      setState((){
+        _counter = 'error in remove: $e';
+      });
+    }
   }
 
   Future<void> editProfile() async
@@ -78,7 +128,9 @@ class _HomePageState extends State<HomePage> {
     try {
       FirebaseFirestore db = FirebaseFirestore.instance;
       CollectionReference profileRef = db.collection("profile_data");
-      DocumentSnapshot qs = await profileRef.doc("svVm2V1l9YQCqJYH0y0Jpg3iapz2").get();
+      final FirebaseAuth auth = FirebaseAuth.instance;
+      User? currentUser;
+      DocumentSnapshot qs = await profileRef.doc(auth.currentUser?.uid).get();
 
       if(qs.exists)
         {
@@ -97,9 +149,9 @@ class _HomePageState extends State<HomePage> {
         }else
           {
             setState(() {
-              _counter = "Profile not found for user svVm2V1l9YQCqJYH0y0Jpg3iapz2";
+              _counter = "Profile not found for user";
             });
-            print("Profile not found for user svVm2V1l9YQCqJYH0y0Jpg3iapz2");
+            print("Profile not found for user ");
           }
     } catch (e) {
       setState(() {
