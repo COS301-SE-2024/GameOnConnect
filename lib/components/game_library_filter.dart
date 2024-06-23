ecee39800c540557eb35b53e9b00f7eb24dd5f04
franco-dreyer <u22520016@tuks.co.za>
2024-06-23 22:29:22 +0200

    Updated game library
    I added a lot of changes to the Game Library, including callback functions, state changes and global keys.  These changes now allow the filter button to concatenate a filter string to send to the api and display on the game page.

--- lib/components/game_library_filter.dart
+++ lib/components/game_library_filter.dart
@@ -1,22 +1,34 @@
 import 'package:expandable/expandable.dart';
 import 'package:flutter/material.dart';
-import 'package:gameonconnect/model/game_filters.dart'; 
-import 'package:gameonconnect/model/game_filter.dart'; 
+import 'package:gameonconnect/model/game_filters.dart';
+import 'package:gameonconnect/model/game_filter.dart';
 
 class FilterPage extends StatefulWidget {
-  const FilterPage({super.key});
+  const FilterPage({Key? key}) : super(key: key);
 
   @override
   State<FilterPage> createState() => _FilterPageState();
 }
 
 class _FilterPageState extends State<FilterPage> {
-  final ExpandableController _platformExpandableController = ExpandableController();
-  final ExpandableController _storeExpandableController = ExpandableController();
-  final ExpandableController _genreExpandableController = ExpandableController();
+  final ExpandableController _platformExpandableController =
+      ExpandableController();
+  final ExpandableController _storeExpandableController =
+      ExpandableController();
+  final ExpandableController _genreExpandableController =
+      ExpandableController();
   final ExpandableController _tagExpandableController = ExpandableController();
 
   late Future<FilterList> _filterListFuture;
+
+  final GlobalKey<_ExpandableFilterState> _platformFilterKey =
+      GlobalKey<_ExpandableFilterState>();
+  final GlobalKey<_ExpandableFilterState> _genreFilterKey =
+      GlobalKey<_ExpandableFilterState>();
+  final GlobalKey<_ExpandableFilterState> _storeFilterKey =
+      GlobalKey<_ExpandableFilterState>();
+  final GlobalKey<_ExpandableFilterState> _tagFilterKey =
+      GlobalKey<_ExpandableFilterState>();
 
   @override
   void initState() {
@@ -24,98 +36,122 @@ class _FilterPageState extends State<FilterPage> {
     _filterListFuture = FilterList.getInstance();
   }
 
+  void _updateFilterString() {
+    setState(() {});
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          backgroundColor: Theme.of(context).colorScheme.surface,
-          automaticallyImplyLeading: false,
-          leading: IconButton(
-            icon: const Icon(Icons.arrow_back),
-            onPressed: () async {
-              Navigator.pop(context);
-            },
-          ),
-          title: Text('Filter',
-              style: TextStyle(
-                fontSize: 32,
-                fontWeight: FontWeight.bold,
-                color: Theme.of(context).colorScheme.onSurface,
-              )),
-          actions: const [],
-          centerTitle: false,
-          elevation: 0,
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.surface,
+        automaticallyImplyLeading: false,
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back),
+          onPressed: () async {
+            Navigator.pop(context);
+          },
         ),
-        body: SafeArea(
-          top: true,
-          child: FutureBuilder<FilterList>(
-            future: _filterListFuture,
-            builder: (context, snapshot) {
-              if (snapshot.connectionState == ConnectionState.waiting) {
-                return Center(child: CircularProgressIndicator());
-              } else if (snapshot.hasError) {
-                return Center(child: Text('Error: ${snapshot.error}'));
-              } else if (snapshot.hasData) {
-                return filterList(context, snapshot.data!);
-              } else {
-                return Center(child: Text('No data'));
-              }
-            },
+        title: Text(
+          'Filter',
+          style: TextStyle(
+            fontSize: 32,
+            fontWeight: FontWeight.bold,
+            color: Theme.of(context).colorScheme.onSurface,
           ),
-        )
+        ),
+        actions: const [],
+        centerTitle: false,
+        elevation: 0,
+      ),
+      body: SafeArea(
+        top: true,
+        child: FutureBuilder<FilterList>(
+          future: _filterListFuture,
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return Center(child: CircularProgressIndicator());
+            } else if (snapshot.hasError) {
+              return Center(child: Text('Error: ${snapshot.error}'));
+            } else if (snapshot.hasData) {
+              return filterList(context, snapshot.data!);
+            } else {
+              return Center(child: Text('No data'));
+            }
+          },
+        ),
+      ),
     );
   }
 
   ListView filterList(BuildContext context, FilterList filterList) {
     return ListView(
-      padding: EdgeInsets.zero,
-      scrollDirection: Axis.vertical,
-      children: [
-        ExpandableFilter(
-          platformExpandableController: _platformExpandableController,
-          filterName: "Platforms",
-          filterValues: filterList.platformFilters.map((filter) => filter.value).toList(),
-        ),
-        ExpandableFilter(
-          platformExpandableController: _genreExpandableController,
-          filterName: "Genres",
-          filterValues: filterList.genreFilters.map((filter) => filter.value).toList(),
-        ),
-        ExpandableFilter(
-          platformExpandableController: _storeExpandableController,
-          filterName: "Stores",
-          filterValues: filterList.storeFilters.map((filter) => filter.value).toList(),
-        ),
-        ExpandableFilter(
-          platformExpandableController: _tagExpandableController,
-          filterName: "Tags",
-          filterValues: filterList.tagFilters.map((filter) => filter.value).toList(),
-        ),
-        Padding(
-          padding: const EdgeInsets.all(10),
-          child: FilledButton(
-            onPressed: () {
-              // Handle filter button click
-            },
-            child: const Text("Filter"),
+        padding: EdgeInsets.zero,
+        scrollDirection: Axis.vertical,
+        children: [
+          ExpandableFilter(
+            key: _platformFilterKey,
+            platformExpandableController: _platformExpandableController,
+            filterName: "Platforms",
+            filterValues: filterList.platformFilters.toList(),
+            onFilterChanged: _updateFilterString,
           ),
-        )
-      ],
-    );
+          ExpandableFilter(
+            key: _genreFilterKey,
+            platformExpandableController: _genreExpandableController,
+            filterName: "Genres",
+            filterValues: filterList.genreFilters.toList(),
+            onFilterChanged: _updateFilterString,
+          ),
+          ExpandableFilter(
+            key: _storeFilterKey,
+            platformExpandableController: _storeExpandableController,
+            filterName: "Stores",
+            filterValues: filterList.storeFilters.toList(),
+            onFilterChanged: _updateFilterString,
+          ),
+          ExpandableFilter(
+            key: _tagFilterKey,
+            platformExpandableController: _tagExpandableController,
+            filterName: "Tags",
+            filterValues: filterList.tagFilters.toList(),
+            onFilterChanged: _updateFilterString,
+          ),
+          Padding(
+            padding: const EdgeInsets.all(10),
+            child: ElevatedButton(
+              onPressed: _applyFilters,
+              child: const Text("Filter"),
+            ),
+          ),
+        ]);
+  }
+
+  void _applyFilters() {
+    String concatenatedFilterString = [
+      _platformFilterKey.currentState?.getFilterString() ?? '',
+      _genreFilterKey.currentState?.getFilterString() ?? '',
+      _storeFilterKey.currentState?.getFilterString() ?? '',
+      _tagFilterKey.currentState?.getFilterString() ?? '',
+    ].where((filter) => filter.isNotEmpty).join();
+
+    print(concatenatedFilterString);
   }
 }
 
 class ExpandableFilter extends StatefulWidget {
   const ExpandableFilter({
-    super.key,
+    Key? key,
     required this.platformExpandableController,
     required this.filterName,
     required this.filterValues,
-  });
+    required this.onFilterChanged,
+  }) : super(key: key);
 
   final ExpandableController platformExpandableController;
   final String filterName;
-  final List<String> filterValues;
+  final List<Filter> filterValues;
+  final VoidCallback onFilterChanged;
 
   @override
   State<ExpandableFilter> createState() => _ExpandableFilterState();
@@ -123,13 +159,32 @@ class ExpandableFilter extends StatefulWidget {
 
 class _ExpandableFilterState extends State<ExpandableFilter> {
   late String _filterName;
-  late List<String> _filterValues;
+  late List<Filter> _filterValues;
+  final List<String> _selectedValues = [];
 
   @override
   void initState() {
     super.initState();
     _filterName = widget.filterName;
     _filterValues = widget.filterValues;
+  }
+
+  String getFilterString() {
+    if (_selectedValues.isEmpty) {
+      return "";
+    }
+    return "&${_filterName.toLowerCase()}=${_selectedValues.join(',')}";
+  }
+
+  void _onCheckboxChanged(bool? newValue, String value) {
+    setState(() {
+      if (newValue == true) {
+        _selectedValues.add(value);
+      } else {
+        _selectedValues.remove(value);
+      }
+      widget.onFilterChanged();
+    });
   }
 
   @override
@@ -172,7 +227,7 @@ class _ExpandableFilterState extends State<ExpandableFilter> {
     );
   }
 
-  List<Widget> _buildCheckboxList(BuildContext context, List<String> values) {
+  List<Widget> _buildCheckboxList(BuildContext context, List<Filter> values) {
     return values.map((value) {
       return Theme(
         data: ThemeData(
@@ -186,11 +241,9 @@ class _ExpandableFilterState extends State<ExpandableFilter> {
           unselectedWidgetColor: Theme.of(context).colorScheme.tertiary,
         ),
         child: CheckboxListTile(
-          value: false,
-          onChanged: (newValue) async {
-            // Change the checkbox state
-          },
-          title: Text(value),
+          value: _selectedValues.contains(value.slug),
+          onChanged: (newValue) => _onCheckboxChanged(newValue, value.slug),
+          title: Text(value.value),
           tileColor: Theme.of(context).colorScheme.surface,
           activeColor: Theme.of(context).colorScheme.primary,
           checkColor: Theme.of(context).colorScheme.surface,
