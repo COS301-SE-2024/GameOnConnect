8af4a62dba922cc844bf9c283021437f9b52a2fb
LeratoKgomo <162160922+LeratoKgomo@users.noreply.github.com>
2024-07-10 17:10:27 +0200

    added Number of  connections

--- lib/model/profile_M/profile_model.dart
+++ lib/model/profile_M/profile_model.dart
@@ -2,18 +2,24 @@ class Profile {
   final String banner;
   final String bio;
   final String profilePicture;
-  final Map<String, dynamic> username; // { 'profilename': 'JohnDoe', 'uniqueNumber': 123 }
+  final Map<String, dynamic> userName; // { 'profilename': 'JohnDoe', 'uniqueNumber': 123 }
+  final String profileName;
+  final double uniqueNumber;// since it has a larger range
   final String currentlyPlaying;
   final List<String> myGames;
   final List<String> wantToPlay;
+  final int numberOfconnections; 
 
   Profile({
     required this.banner,
     required this.bio,
     required this.profilePicture,
-    required this.username,
+    required this.userName,
+    required this.profileName,
+    required this.uniqueNumber,
     required this.currentlyPlaying,
     required this.myGames,
     required this.wantToPlay,
+    required this.numberOfconnections,
   });
 }
