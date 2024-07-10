1090699617ee72c355d6fc24a06c3011539ddfaa
LeratoKgomo <162160922+LeratoKgomo@users.noreply.github.com>
2024-07-10 17:17:14 +0200

    get number of connections from list

--- lib/services/profile_S/profile_service.dart
+++ lib/services/profile_S/profile_service.dart
@@ -3,6 +3,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 //import 'package:gameonconnect/view/pages/profile/profile_page.dart';
 import 'package:gameonconnect/model/profile_M/profile_model.dart';
+import 'package:gameonconnect/services/connection_S/connection_service.dart';
 
 class ProfileService {
 
@@ -20,14 +21,26 @@ Future<Profile?>  fetchProfile() async {
         if (doc.exists) {
           Map<String, dynamic> data = doc.data() as Map<String, dynamic>;
 
+          Map<String, dynamic> userInfo =data['username'] as Map<String, dynamic>;
+          String name= userInfo['profile_name'] ?? 'username';
+          double uniqueNum=userInfo['unique_num'] ?? '';
+
+  	      // Get number of connections
+        ConnectionService connectionService = ConnectionService();
+         List<String> connections= await ConnectionService().getConnections("connections");
+        int connectionsCount = connections.length;
+
           return Profile(
           banner: data['banner'] ?? '',
           bio: data['bio'] ?? '',
           profilePicture: data['profile_picture'] ?? '',
-          username: Map<String, dynamic>.from(data['username'] ?? {}),
+          userName: data['username'] as Map<String, dynamic>,
+          profileName: name, 
+          uniqueNumber: uniqueNum,
           currentlyPlaying: data['currently_playing'] ?? '',
           myGames: List<String>.from(data['my_games'] ?? []),
           wantToPlay: List<String>.from(data['want_to_play'] ?? []),
+          numberOfconnections: connectionsCount,
       );
         } else {
           print('Document not found');
@@ -40,6 +53,7 @@ Future<Profile?>  fetchProfile() async {
     }
     return null;
   }
+
 
 }
 
