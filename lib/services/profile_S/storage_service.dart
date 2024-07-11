b343a789753b1cf8c7f54e55962503dc2862bc38
LeratoKgomo <162160922+LeratoKgomo@users.noreply.github.com>
2024-07-11 16:17:07 +0200

    added storage services for banner and profile picture

---
+++ lib/services/profile_S/storage_service.dart
@@ -0,0 +1,30 @@
+import 'package:firebase_storage/firebase_storage.dart';
+
+class StorageService {
+  final FirebaseStorage _storage = FirebaseStorage.instance;
+
+  Future<String> _getImageUrl(String imageType,String path) async {
+    try {
+      return await _storage.ref(path).getDownloadURL();
+    } catch (e) {
+      print(e);
+      if(imageType=='banner')
+      {
+        return await _storage.ref('banners/default_bannner.jpg').getDownloadURL();
+      }
+      else//profile picture 
+      {
+        return await _storage.ref('profile_pictures/default_image.jpg').getDownloadURL();
+      }
+
+    }
+  }
+
+  Future<String> getBannerUrl(String userId) async {
+    return _getImageUrl('banner','banners/$userId.jpg');
+  }
+
+  Future<String> getProfilePictureUrl(String userId) async {
+    return _getImageUrl('profilePicture','profile_pictures/$userId.jpg');
+  }
+}
