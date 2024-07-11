640f87d79a34ca96e94200dd8090bfaa76d78457
Pieryvdl <pieryvanderlinde@gmail.com>
2024-07-12 00:04:44 +0200

    removed unused imports

--- test/feedpage_test.dart
+++ test/feedpage_test.dart
@@ -1,6 +1,6 @@
-import 'package:flutter/material.dart';
+//import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:gameonconnect/view/pages/feed/feed_page.dart';
+//import 'package:gameonconnect/view/pages/feed/feed_page.dart';
 
 void main() {
   group('HomePage Widget Tests', () {
