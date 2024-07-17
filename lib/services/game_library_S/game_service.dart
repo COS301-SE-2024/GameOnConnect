12e5240cb5bd5a9a51c2b633a690936e06f99fa8
Shannon <131780205+shan-226@users.noreply.github.com>
2024-07-17 18:28:11 +0200

    ⚰️ removed extra commented out code

--- lib/services/game_library_S/game_service.dart
+++ lib/services/game_library_S/game_service.dart
@@ -37,35 +37,3 @@ class GameService {
     }
   }
 }
-
-
-
-
-
-
-
-
-
-
-// import 'dart:convert';
-// import 'dart:io';
-// import '../../model/game_library_M/game_details_model.dart';
-// import 'package:http/http.dart' as http;
-
-// class GameService{
-// Future<GameDetails> fetchGameDetails( gameID) async {
-//   try {
-//     final response = await http.get(Uri.parse(
-//         'https://api.rawg.io/api/games/$gameID?key=b8d81a8e79074f1eb5c9961a9ffacee6'));
-//     if (response.statusCode == 200) {
-//       final jsonResponse = jsonDecode(response.body);
-//       print('Full JSON response: $jsonResponse'); // Debugging statement
-//       return GameDetails.fromJson(jsonDecode(response.body));
-//     } else {
-//       throw Exception('Failed to load game details');
-//     }
-//   } on SocketException {
-//     throw Exception('No Internet connection');
-//   }
-// }
-// }
