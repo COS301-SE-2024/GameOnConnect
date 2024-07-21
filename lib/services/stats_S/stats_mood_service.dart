e1ce6fc9d5a33c719b22b8ffe40f76ae55541558
Shannon <131780205+shan-226@users.noreply.github.com>
2024-07-21 23:41:57 +0200

    🔨 added function to stats mood service

--- lib/services/stats_S/stats_mood_service.dart
+++ lib/services/stats_S/stats_mood_service.dart
@@ -32,60 +32,18 @@ class StatsMoodService {
       'fear': moods.where((mood) => mood == 'fear').length,
     };
   }
+
+  Future<List<String>> fetchGameIDsForMood(String mood) async {
+    try {
+      QuerySnapshot querySnapshot = await _firestore
+          .collection('stats')
+          .where('mood', isEqualTo: mood)
+          .get();
+
+      List<String> gameIDs = querySnapshot.docs.map((doc) => doc['gameID'] as String).toList();
+      return gameIDs;
+    } catch (e) {
+      throw Exception('Error fetching game IDs for mood: $e');
+    }
+  }
 }
-
-
-
-
-
-
-
-
-
-
-
-// import 'package:cloud_firestore/cloud_firestore.dart';
-// import 'package:firebase_auth/firebase_auth.dart';
-
-// class StatsMoodService {
-//   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
-//   final FirebaseAuth _auth = FirebaseAuth.instance;
-
-//   Future<Map<String, int>> fetchMoodData() async {
-//     User? currentUser = _auth.currentUser;
-//     if (currentUser == null) {
-//       return {
-//         'joy': 0,
-//         'disgust': 0,
-//         'sad': 0,
-//         'angry': 0,
-//         'fear': 0,
-//       };
-//     }
-
-//     final snapshot = await _firestore
-//         .collection('stats')
-//         .where('userID', isEqualTo: currentUser.uid)
-//         .get();
-
-//     if (snapshot.docs.isEmpty) {
-//       return {
-//         'joy': 0,
-//         'disgust': 0,
-//         'sad': 0,
-//         'angry': 0,
-//         'fear': 0,
-//       };
-//     }
-
-//     final moods = snapshot.docs.map((doc) => doc['mood']).toList();
-
-//     return {
-//       'joy': moods.where((mood) => mood == 'joy').length,
-//       'disgust': moods.where((mood) => mood == 'disgust').length,
-//       'sad': moods.where((mood) => mood == 'sad').length,
-//       'angry': moods.where((mood) => mood == 'angry').length,
-//       'fear': moods.where((mood) => mood == 'fear').length,
-//     };
-//   }
-// }
