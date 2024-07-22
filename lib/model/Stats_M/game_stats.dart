d073f25132f1325b08011db9c15ccf7f45444ee8
LeratoKgomo <162160922+LeratoKgomo@users.noreply.github.com>
2024-07-22 19:03:28 +0200

    updated Gamestats model

--- lib/model/Stats_M/game_stats.dart
+++ lib/model/Stats_M/game_stats.dart
@@ -1,23 +1,14 @@
 class GameStats{
   final String gameId;
-  final String lastPlayed;
-  final String mood; // In hours
-  final String timePlayedLast;
+  final String lastPlayedDate;
+  final String mood; 
+  final int timePlayedLast;
 
   GameStats({
     required this.gameId,
-    required this.lastPlayed,
+    required this.lastPlayedDate,
     required this.mood,
     required this.timePlayedLast
   });
 }
 
-//DateTime firestoreDateTime = snapshot.data.documents[index].data['timestamp'].toDate();
-/**
- * import 'package:intl/intl.dart';
-
-// Assuming you already have the Firestore timestamp as 'firestoreDateTime'
-String formattedDate = DateFormat.yMMMd().format(firestoreDateTime); // Format date
-int hour = firestoreDateTime.hour; // Hour
-int minute = firestoreDateTime.minute; 
- */
