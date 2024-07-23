d81b4b537f7ec2364b207a0ab2a5f85628dc4678
Shannon <131780205+shan-226@users.noreply.github.com>
2024-07-23 22:14:13 +0200

    📸 updated service

--- lib/services/stats_S/stats_games_service.dart
+++ lib/services/stats_S/stats_games_service.dart
@@ -1,23 +1,30 @@
 import 'dart:convert';
 import 'dart:io';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:http/http.dart' as http;
 import '../../model/game_library_M/game_details_model.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class StatsGamesService {
-  Future<List<String>> fetchGameImages(List<String> gameIDs) async {
-    List<String> gameImages = [];
+  Future<List<Map<String, String>>> fetchGameImages(List<Map<String, dynamic>> gameData) async {
+    List<Map<String, String>> gameImages = [];
 
     String? apikey = dotenv.env['RAWG_API_KEY'];
 
-    for (String gameID in gameIDs) {
+    for (Map<String, dynamic> game in gameData) {
+      String gameID = game['gameID'];
+      String lastPlayed = (game['last_played'] as Timestamp).toDate().toString();
+
       try {
         final response = await http.get(Uri.parse(
             'https://api.rawg.io/api/games/$gameID?key=$apikey'));
         if (response.statusCode == 200) {
           GameDetails gameDetails =
               GameDetails.fromJson(jsonDecode(response.body));
-          gameImages.add(gameDetails.backgroundImage);
+          gameImages.add({
+            'imageUrl': gameDetails.backgroundImage,
+            'lastPlayed': lastPlayed,
+          });
         } else {
           throw Exception('Failed to load game details');
         }
@@ -25,6 +32,6 @@ class StatsGamesService {
         throw Exception('No Internet connection');
       }
     }
-    return gameImages;
+    return gameImages;   //returns image urls and timestamp
   }
 }
