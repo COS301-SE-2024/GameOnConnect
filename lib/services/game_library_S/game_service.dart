e6a3b1720d9ea14ba431b1752d584af8a5660a8d
LeratoKgomo <162160922+LeratoKgomo@users.noreply.github.com>
2024-07-24 08:37:12 +0200

    updated API key

--- lib/services/game_library_S/game_service.dart
+++ lib/services/game_library_S/game_service.dart
@@ -1,20 +1,26 @@
 import 'dart:convert';
 import 'dart:io';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
 import '../../model/game_library_M/game_details_model.dart';
 import 'package:http/http.dart' as http;
 
-class GameService{
-Future<GameDetails> fetchGameDetails( gameID) async {
-  try {
-    final response = await http.get(Uri.parse(
-        'https://api.rawg.io/api/games/$gameID?key=b8d81a8e79074f1eb5c9961a9ffacee6'));
-    if (response.statusCode == 200) {
-      return GameDetails.fromJson(jsonDecode(response.body));
-    } else {
-      throw Exception('Failed to load game details');
+class GameService {
+  final String? apiKey = dotenv.env['RAWG_API_KEY'];
+
+  Future<GameDetails> fetchGameDetails( gameId) async {
+    try {
+      final response = await http.get(Uri.parse(
+          'https://api.rawg.io/api/games/$gameId?key=$apiKey'));
+      if (response.statusCode == 200) {
+        final jsonResponse = jsonDecode(response.body);
+        return GameDetails.fromJson(jsonResponse);
+      } else {
+        throw Exception('Failed to load game details');
+      }
+    } on SocketException {
+      throw Exception('No Internet connection');
     }
-  } on SocketException {
-    throw Exception('No Internet connection');
   }
-}
+
 }
