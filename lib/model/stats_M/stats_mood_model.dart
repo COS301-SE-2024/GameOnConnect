23adc49473ed49499903860f93a61224f25a37c3
Shannon <131780205+shan-226@users.noreply.github.com>
2024-07-24 22:46:14 +0200

    🔨  created model

---
+++ lib/model/stats_M/stats_mood_model.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+class Indicator extends StatelessWidget {
+  final Color color;
+  final String text;
+  final bool isSquare;
+  final double size;
+  final Color textColor;
+
+  const Indicator({
+    super.key,
+    required this.color,
+    required this.text,
+    this.isSquare = true,
+    this.size = 16,
+    this.textColor = const Color(0xff505050),
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    // Theme.of(context);
+    return Row(
+      children: <Widget>[
+        Container(
+          width: size,
+          height: size,
+          decoration: BoxDecoration(
+            shape: isSquare ? BoxShape.rectangle : BoxShape.circle,
+            color: color,
+          ),
+        ),
+        const SizedBox(width: 4),
+        Text(
+          text,
+          style: TextStyle(
+            fontSize: 16,
+            fontWeight: FontWeight.bold,
+            color: textColor,
+          ),
+        )
+      ],
+    );
+  }
+}
