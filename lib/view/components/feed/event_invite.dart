2f26dc6e29a948344a19caea5230b2e2b7a4537d
franco-dreyer <u22520016@tuks.co.za>
2024-07-25 11:31:44 +0200

    Started styling the event invites

--- lib/view/components/feed/event_invite.dart
+++ lib/view/components/feed/event_invite.dart
@@ -38,64 +38,131 @@ class _EventInvitationState extends State<EventInvitation> {
   @override
   Widget build(BuildContext context) {
     if (_isWidgetVisible) {
-    return Expanded(
-      child: ExpansionTile(
-        title: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text('${widget.inviterName} invited you to join',
-                style: const TextStyle(fontWeight: FontWeight.bold)),
-            Text(widget.event.name,
-                style: const TextStyle(
-                    fontWeight: FontWeight.bold, color: Colors.green)),
-          ],
-        ),
-        children: <Widget>[
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                'Description: ${widget.event.description}',
+      return Padding(
+        padding: const EdgeInsets.all(15),
+        child: Expanded(
+          child: Container(
+            padding: const EdgeInsets.only(top: 5, right: 15, left: 15, bottom: 5),
+            decoration: BoxDecoration(
+              color: Color.fromARGB(255, 243, 243, 243),
+              borderRadius: BorderRadius.circular(15),
+            ),
+            child: Theme(
+              data: Theme.of(context).copyWith(dividerColor: Colors.transparent),
+              child: ExpansionTile(
+                title: Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Icon(
+                      Icons.calendar_month_rounded,
+                      color: Theme.of(context).colorScheme.primary,
+                      size: 24,
+                    ),
+                    const SizedBox(width: 15),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text('${widget.inviterName} invited you to join',
+                            style: const TextStyle(fontWeight: FontWeight.bold)),
+                        Text(widget.event.name,
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                color: Theme.of(context).colorScheme.primary)),
+                      ],
+                    ),
+                  ],
+                ),
+                children: <Widget>[
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Description: ${widget.event.description}',
+                      ),
+                      Text(
+                        'Date: ${widget.event.startDate}',
+                      )
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      InkWell(
+                        //accept
+                        onTap: () {
+                          _eventsService.declineEventInvitation(widget.event);
+                          _eventsService.joinEvent(widget.event);
+                          setState(() {
+                            _isWidgetVisible = false;
+                          });
+                        },
+                        child: Container(
+                            alignment: Alignment.center,
+                            height: 30,
+                            width: 85,
+                            decoration: BoxDecoration(
+                              color: Theme.of(context).colorScheme.primary,
+                              borderRadius: BorderRadius.circular(7),
+                            ),
+                            child: const Text('Accept',
+                                style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 12,
+                                ))),
+                      ),
+                      InkWell(
+                          //decline
+                          onTap: () {
+                            _eventsService.declineEventInvitation(widget.event);
+                            setState(() {
+                              _isWidgetVisible = false;
+                            });
+                          },
+                          child: Container(
+                              alignment: Alignment.center,
+                              height: 30,
+                              width: 85,
+                              decoration: BoxDecoration(
+                                color: Theme.of(context).colorScheme.primary,
+                                borderRadius: BorderRadius.circular(7),
+                              ),
+                              child: const Text('Decline',
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 12,
+                                  )))),
+                      InkWell(
+                          //stay notified
+                          onTap: () {
+                            _eventsService.declineEventInvitation(widget.event);
+                            _eventsService.subscribeToEvent(widget.event);
+                            setState(() {
+                              _isWidgetVisible = false;
+                            });
+                          },
+                          child: Container(
+                              alignment: Alignment.center,
+                              height: 30,
+                              width: 85,
+                              decoration: BoxDecoration(
+                                border: Border.all(
+                                    color: Theme.of(context).colorScheme.primary),
+                                borderRadius: BorderRadius.circular(7),
+                              ),
+                              child: const Text('Stay notified',
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 12,
+                                  )))),
+                    ],
+                  )
+                ],
               ),
-              Text(
-                'Date: ${widget.event.startDate}',
-              )
-            ],
+            ),
           ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              FilledButton(
-                  onPressed: () {
-                    _eventsService.declineEventInvitation(widget.event);
-                    _eventsService.joinEvent(widget.event);
-                    setState(() {
-                      _isWidgetVisible = false;
-                    });
-                  },
-                  child: const Text('Accept')),
-              FilledButton(
-                  onPressed: () {
-                    _eventsService.declineEventInvitation(widget.event);
-                    setState(() {
-                      _isWidgetVisible = false;
-                    });
-                  },
-                  child: const Text('Decline')),
-              FilledButton(
-                  onPressed: () {
-                    _eventsService.declineEventInvitation(widget.event);
-                    _eventsService.subscribeToEvent(widget.event);
-                    setState(() {
-                      _isWidgetVisible = false;
-                    });
-                  },
-                  child: const Text('Stay notified')),
-            ],
-          )
-        ],
-      ),
-    );
+        ),
+      );
     } else {
       return const SizedBox(height: 0);
     }
