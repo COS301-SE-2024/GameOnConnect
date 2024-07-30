68b8b9d57aa8a4df72fa760c29b6509bcd3a1463
LeratoKgomo <162160922+LeratoKgomo@users.noreply.github.com>
2024-07-30 15:32:47 +0200

    updated class required fields

--- lib/view/pages/profile/connections_list.dart
+++ lib/view/pages/profile/connections_list.dart
@@ -1,12 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:gameonconnect/model/connection_M/user_model.dart'as user;
+import 'package:gameonconnect/services/connection_S/connection_service.dart';
 import 'package:gameonconnect/services/events_S/event_service.dart';
 import 'package:gameonconnect/view/components/card/connection_list_card.dart';
 import 'package:gameonconnect/view/pages/profile/connections_request_list.dart';
 
 class ConnectionsList extends StatefulWidget {
-  const ConnectionsList({super.key, });
-  //final List<User> users;
+  const ConnectionsList({
+    super.key,
+    required this.isOwnProfile,// true
+    required this.uid,// u
+     });
+
+    final bool isOwnProfile;
+    final String uid;
 
   @override
   State<ConnectionsList> createState() => _ConnectionsListState();
@@ -51,62 +58,64 @@ class _ConnectionsListState extends State<ConnectionsList> {
     ),
     
       body:  FutureBuilder<List<user.AppUser>?>(
-              future: EventsService().getConnectionsForInvite(),
+              future:  ConnectionService().getProfileConnections('connections',widget.uid),
               builder: (context, snapshot) {
                 if (snapshot.connectionState == ConnectionState.waiting) {
                   return const Center(child: CircularProgressIndicator());
                 } else if (snapshot.hasError) {
-                  return Text('Error: ${snapshot.error}');
+                  return Text('Error: ${snapshot.error}, ');
                 } else {
                   list = snapshot.data;
                   if (list!.isEmpty) {
-            // Display "No connections" when the list is empty
-            return Column(
+                  // Display "No connections" when the list is empty
+                  return Column(
                     mainAxisSize: MainAxisSize.max, 
                     children: [
+                    if (widget.isOwnProfile)
                     GestureDetector(
-          onTap: () {
-            // Navigate to the request page when the text is clicked
-            Navigator.push(context, MaterialPageRoute(builder: (context) => const ConnectionRequestList()));//go to next page 
-          },
-          child: Container(
-            padding: const EdgeInsets.symmetric(horizontal: 16),
-            alignment: Alignment.centerRight,
-            child: Text(
-              'Requests',
-              style: TextStyle(
-                color: Theme.of(context).colorScheme.primary, // Customize the text color
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-          ),
-        ), 
-            const Center(
-              child: Text('No Connections'),
-            )
-            ]);
-          } else{
-            return Column(
-                    mainAxisSize: MainAxisSize.max, 
-                    children: [
-                    GestureDetector(
-          onTap: () {
-            // Navigate to the request page when the text is clicked
-            Navigator.push(context, MaterialPageRoute(builder: (context) => const ConnectionRequestList()));//go to next page 
-          },
-          child: Container(
-            padding: const EdgeInsets.symmetric(horizontal: 16),
-            alignment: Alignment.centerRight,
-            child: Text(
-              'Requests',
-              style: TextStyle(
-                color: Theme.of(context).colorScheme.primary, // Customize the text color
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-          ),
-        ),
-                    SizedBox(
+                      onTap: () {
+                        // Navigate to the request page when the text is clicked
+                        Navigator.push(context, MaterialPageRoute(builder: (context) =>  ConnectionRequestList(isOwnProfile: widget.isOwnProfile,uid : widget.uid)));//go to next page 
+                      },
+                      child: Container(
+                        padding: const EdgeInsets.symmetric(horizontal: 16),
+                        alignment: Alignment.centerRight,
+                        child: Text(
+                          'Requests',
+                          style: TextStyle(
+                            color: Theme.of(context).colorScheme.primary, // Customize the text color
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                    ), 
+                       Center(
+                        child: Text('No Connections ${widget.uid}, ${widget.isOwnProfile}'),
+                      )
+                      ]);
+                    } else{
+                      return Column(
+                        mainAxisSize: MainAxisSize.max, 
+                        children: [
+                        if (widget.isOwnProfile)
+                        GestureDetector(
+                          onTap: () {
+                            // Navigate to the request page when the text is clicked
+                            Navigator.push(context, MaterialPageRoute(builder: (context) =>  ConnectionRequestList(isOwnProfile: widget.isOwnProfile,uid : widget.uid)));//go to next page 
+                          },
+                          child: Container(
+                            padding: const EdgeInsets.symmetric(horizontal: 16),
+                            alignment: Alignment.centerRight,
+                            child: Text(
+                              'Requests',
+                              style: TextStyle(
+                                color: Theme.of(context).colorScheme.primary, // Customize the text color
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
+                        ),
+                      SizedBox(
                         height: 300,
                         child: ListView.separated(
                           itemCount: list!.length,
