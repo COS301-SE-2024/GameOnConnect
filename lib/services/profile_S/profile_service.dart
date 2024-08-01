0df9cc49c9a7bf4f077c684ced5c68fad8a44898
franco-dreyer <u22520016@tuks.co.za>
2024-08-02 01:10:45 +0200

    Fixed currently_playing being set
    I fixed the code so that the currently playing field in the database is actually set to something when the user starts the timer.  It also obviously sets it to an empty string when the user STOPS the timer.

--- lib/services/profile_S/profile_service.dart
+++ lib/services/profile_S/profile_service.dart
@@ -78,6 +78,24 @@ Future<Profile?>  fetchProfile() async {
     }
   }
 
+  Future<void> setCurrentlyPlaying(String gameId) async {
+    FirebaseFirestore db = FirebaseFirestore.instance;
+    final FirebaseAuth auth = FirebaseAuth.instance;
+    final currentUser = auth.currentUser;
+
+    if (currentUser != null) {
+      DocumentReference doc = db.collection('profile_data').doc(currentUser.uid);
+
+      try {
+        await doc.update({'currently_playing': gameId});
+      } catch (e) {
+        print("Error setting currently playing: $e");
+      }
+    } else {
+      throw(Exception('User not found'));
+    }
+  }
+
   Future<List<GameStats>> fetchRecentActivities(List<DocumentReference> recentActivitiesRefs) async {
  final recentActivities = <GameStats>[];
 
