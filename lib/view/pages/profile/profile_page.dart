707a14e25b8894d9462e288f07aef5cf1161ddd7
LeratoKgomo <162160922+LeratoKgomo@users.noreply.github.com>
2024-08-05 20:14:57 +0200

    show profile number : games, connections, total played

--- lib/view/pages/profile/profile_page.dart
+++ lib/view/pages/profile/profile_page.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:gameonconnect/model/profile_M/profile_model.dart';
 import 'package:gameonconnect/services/connection_S/connection_service.dart';
 import 'package:gameonconnect/services/profile_S/profile_service.dart';
+import 'package:gameonconnect/view/components/profile/profile_buttons.dart';
 import 'package:gameonconnect/view/pages/profile/connections_list.dart';
 import 'package:gameonconnect/view/pages/profile/currently_playing.dart';
 import 'package:gameonconnect/view/pages/profile/horizontal_gameslist.dart';
@@ -110,87 +111,41 @@ class _ProfileState extends State<ProfilePage>  {
                             Column(
                               children: [
                                 // Banner
-                                SizedBox(
-                                  height: 170,
+                               SizedBox(
+                                  height: 250,
                                   width: MediaQuery.of(context).size.width,
-                                  child: CachedNetworkImage(
-                                    imageUrl: profileData.banner,
-                                    placeholder: (context, url) => const Center(child: CircularProgressIndicator()), // Loading indicator for banner
-                                    errorWidget: (context, url, error) => const Icon(Icons.error),
-                                    fit: BoxFit.cover,
-                                  ),
-                                ),
-                                // Bottom container
-                                //Center(
-                                Container(
-                                  margin: const EdgeInsets.fromLTRB(50, 0, 3, 13),
-                                  child:Align(
-                                    alignment: Alignment.topLeft,
-                                    child: Column(
-                                      crossAxisAlignment: CrossAxisAlignment.start,
-                                      children: [
-                                        const SizedBox(height: 55), //space 
-                                        Text(
-                                          profileData.profileName,
-                                          style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-                                        ),
-                                        Text(
-                                          '#${profileData.uniqueNumber}',
-                                          style: const TextStyle(fontSize: 16),
-                                        ),
-                                        Text(
-                                          profileData.bio,
-                                          style: const TextStyle(fontSize: 16),
-                                        ),
-                                        const SizedBox(height: 10), //space 
-                                      GestureDetector(
-                                        onTap: () {
-                                          //Navigator.push(context, MaterialPageRoute(builder: (context) =>  ConnectionsList(isOwnProfile: widget.isOwnProfile,))); 
-                                        Navigator.push(context, MaterialPageRoute(builder: (context) =>  ConnectionsList(isOwnProfile: widget.isOwnProfile, uid: widget.uid, loggedInUser: widget.loggedInUser,))); 
-                                        },
-                                        child: RichText(
-                                          text: TextSpan(
-                                            children: [
-                                              TextSpan(
-                                                text: '${profileData.numberOfconnections}',
-                                                style: const TextStyle(
-                                                  color: Colors.black,
-                                                  fontSize: 16,
-                                                  fontWeight: FontWeight.bold,
-                                                ),
-                                              ),
-                                              const TextSpan(
-                                                text: ' Connections',
-                                                style: TextStyle(
-                                                  fontSize: 16,
-                                                   color: Colors.black,
-                                                  ),
-                                              ),
-                                            ],
-                                          ),
-                                        ),
+                                  child: DecoratedBox(
+                                    decoration: BoxDecoration(
+                                    color: Color.fromRGBO(42, 42, 42, 1.0),  
+                                    image: DecorationImage(
+                                      fit: BoxFit.cover,
+                                      //image: CachedNetworkImageProvider(profileData.banner),
+                                      //image:CachedNetworkImageProvider('https://wallup.net/wp-content/uploads/2018/03/19/579265-water-green-nature-waterfall.jpg'),
+                                      image: CachedNetworkImageProvider('https://thumbs.dreamstime.com/b/portrait-young-pretty-female-gamer-playing-shooter-neon-lighting-portrait-young-pretty-female-gamer-playing-272077632.jpg'),
+                                      colorFilter: ColorFilter.mode(
+                                        Colors.grey.withOpacity(0.6), // Adjust opacity here
+                                        BlendMode.dstATop, // Use dstATop for transparency
                                       ),
-                                      ],
                                     ),
                                   ),
+                                  ),
                                 ),
+                      
+                                // Bottom container
+                                //Center(
+                               
                                     //),
                               ],
                             ),
                             // Profile picture
                             Positioned(
-                              top: 120, // Adjust this value to move the profile picture downwards
-                              //left: (MediaQuery.of(context).size.width - 100) / 2, // Center the profile picture
-                              left: 50,
+                              top: 50, // Adjust this value to move the profile picture downwards
+                              left: 19,
                               child:Container(
                                 height: 100,
                                 width: 100,
                                 decoration: BoxDecoration(
                                   shape: BoxShape.circle, // Make it circular
-                                  border: Border.all(
-                                    color: Theme.of(context).colorScheme.primary, // Set your desired border color
-                                    width: 3, // Set the border width
-                                  ),
                                 ),
                                 child: ClipOval(
                                   child: CachedNetworkImage(
@@ -202,9 +157,31 @@ class _ProfileState extends State<ProfilePage>  {
                                 ),
                               ),
                             ),
+                             Positioned(
+                              left: 16,
+                              bottom: 20,
+                              right: 16,
+                              child: Row(
+                                children: [
+                                  Expanded(
+                                    child: ProfileButton(value: '${profileData.myGames.length}', title: 'Games'),
+                                  ),
+                                  Expanded(
+                                    child: ProfileButton(value: '${profileData.numberOfconnections}', title: 'Connections'),
+                                  ),
+                                  const Expanded(
+                                    child: ProfileButton(value: '140 hrs', title: 'Time Played'),
+                                  ),
+                                  
+                                ],
+                              ),
+                              
+                            ),
+                                                  
                           ],
                         ),
-                        if ( profileData.visibility ||isConnectionParent ||widget.uid== widget.loggedInUser)...[
+                        
+                        /*if ( profileData.visibility ||isConnectionParent ||widget.uid== widget.loggedInUser)...[
                           
                           // Conditionally display the CurrentlyPlaying widget
                       profileData.currentlyPlaying.isNotEmpty
@@ -282,7 +259,7 @@ class _ProfileState extends State<ProfilePage>  {
                               ],
                             ),
                           ),
-                        ],
+                        ],*/
                         
                               
                       ],
