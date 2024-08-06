bef8bf6ee76f5dfdaf2a375a613f6157a8cd372a
franco-dreyer <u22520016@tuks.co.za>
2024-08-06 23:13:03 +0200

    Added connection updates page

---
+++ lib/view/components/feed/connecttion_updates.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ConnectionUpdates extends StatefulWidget {
+  const ConnectionUpdates({super.key});
+
+  @override
+  State<ConnectionUpdates> createState() => _ConnectionUpdatesState();
+}
+
+class _ConnectionUpdatesState extends State<ConnectionUpdates> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
