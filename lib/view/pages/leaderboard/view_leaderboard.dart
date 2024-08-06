378733a1da5fde520a2e41e561c43acecdf5ff0d
Pieryvdl <pieryvanderlinde@gmail.com>
2024-08-06 22:55:04 +0200

    complete view_leaderboard page

--- lib/view/pages/leaderboard/view_leaderboard.dart
+++ lib/view/pages/leaderboard/view_leaderboard.dart
@@ -7,7 +7,6 @@ class ViewLeaderboard extends StatefulWidget {
   State<ViewLeaderboard> createState() => _ViewLeaderboardState();
 }
 
-//NB rename
 class _ViewLeaderboardState extends State<ViewLeaderboard> {
   @override
   Widget build(BuildContext context) {
@@ -36,93 +35,303 @@ class _ViewLeaderboardState extends State<ViewLeaderboard> {
           ),
         ],
       ),
-      body: SizedBox(
-        width: double.infinity,
-        child: Padding(
-          padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              const SizedBox(
-                height: 15,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text(
-                    'Poker Tournament',
-                    style: TextStyle(
-                      fontWeight: FontWeight.w700,
-                      fontSize: 25,
-                      color: Theme.of(context).colorScheme.primary,
-                    ),
-                  ),
-                  MaterialButton(
-                    height: 30,
-                    onPressed: () {
-                      //Navigator.push(context, MaterialPageRoute(builder: (context) => const ViewLeaderboard()));
-                    },
-                    color: Theme.of(context).colorScheme.surface,
-                    textColor: Theme.of(context).colorScheme.secondary,
-                    shape: RoundedRectangleBorder(
-                      borderRadius:
-                          BorderRadius.circular(20.0), // Rounded corners
-                      side: BorderSide(
-                        color:
-                            Theme.of(context).colorScheme.primary, // Green edge
-                      ),
-                    ),
-                    child: const Text(
-                      'Edit Scores',
-                      style: TextStyle(
-                        fontSize: 12,
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-              const SizedBox(
-                height: 25,
-              ),
-              Container(
-                margin: const EdgeInsets.fromLTRB(0, 0, 0, 5),
-                child: Container(
-                  decoration: BoxDecoration(
-                    color: const Color(0xFFD9D9D9),
-                    borderRadius: BorderRadius.circular(40),
-                  ),
-                  child: const SizedBox(
-                    width: 60,
-                    height: 60,
-                  ),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const SizedBox(
+                  height: 15,
                 ),
-              ),
-              const SizedBox(
-                height: 10,
-              ),
-              SingleChildScrollView(
-                scrollDirection: Axis.vertical,
-                child: Container(
-                  decoration: BoxDecoration(
-                    color: const Color(0xFFD9D9D9),
-                    borderRadius: BorderRadius.circular(15),
-                  ),
-                  child: Container(
-                    padding: const EdgeInsets.fromLTRB(0, 0, 0, 0),
-                    child: const Column(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      crossAxisAlignment: CrossAxisAlignment.start,
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      'Poker Tournament',
+                      style: TextStyle(
+                        fontWeight: FontWeight.w700,
+                        fontSize: 25,
+                        color: Theme.of(context).colorScheme.primary,
+                      ),
+                    ),
+                    MaterialButton(
+                      height: 30,
+                      minWidth: 30,
+                      onPressed: () {
+                        //Navigator.push(context, MaterialPageRoute(builder: (context) => const ViewLeaderboard()));
+                      },
+                      color: Theme.of(context).colorScheme.surface,
+                      textColor: Theme.of(context).colorScheme.secondary,
+                      shape: RoundedRectangleBorder(
+                        borderRadius:
+                            BorderRadius.circular(20.0), // Rounded corners
+                        side: BorderSide(
+                          color: Theme.of(context)
+                              .colorScheme
+                              .primary, // Green edge
+                        ),
+                      ),
+                      child: const Text(
+                        'Edit Scores',
+                        style: TextStyle(
+                          fontSize: 12,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(
+                  height: 25,
+                ),
+                Row(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(0, 0, 15, 0),
+                      child: Container(
+                        margin: const EdgeInsets.fromLTRB(0, 0, 0, 5),
+                        child: Container(
+                          decoration: BoxDecoration(
+                            color: const Color(0xFFD9D9D9),
+                            borderRadius: BorderRadius.circular(40),
+                          ),
+                          child: const SizedBox(
+                            width: 60,
+                            height: 60,
+                          ),
+                        ),
+                      ),
+                    ),
+                    Expanded(
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                "Team 1",
+                                style: TextStyle(
+                                  fontWeight: FontWeight.w700,
+                                  fontSize: 20,
+                                  color:
+                                      Theme.of(context).colorScheme.secondary,
+                                ),
+                              ),
+                              Text(
+                                "Winning",
+                                style: TextStyle(
+                                  fontSize: 12,
+                                  color:
+                                      Theme.of(context).colorScheme.secondary,
+                                ),
+                              ),
+                            ],
+                          ),
+                          Column(
+                            children: [
+                              Container(
+                                padding: const EdgeInsets.fromLTRB(15, 5, 15, 5),
+                                decoration: BoxDecoration(
+                                  color: Theme.of(context).colorScheme.surface,
+                                  borderRadius: BorderRadius.circular(10.0),
+                                  border: Border.all(
+                                    color:
+                                        Theme.of(context).colorScheme.secondary,
+                                  ),
+                                ),
+                                child: Column(
+                                  children: [
+                                    Text(
+                                      "1st",
+                                      style: TextStyle(
+                                        fontWeight: FontWeight.w700,
+                                        fontFamily: "Inter",
+                                        fontSize: 20,
+                                        color: Theme.of(context)
+                                            .colorScheme
+                                            .secondary,
+                                      ),
+                                    ),
+                                    Text(
+                                      "Rank",
+                                      style: TextStyle(
+                                        fontFamily: "Inter",
+                                        fontSize: 12,
+                                        color: Theme.of(context)
+                                            .colorScheme
+                                            .secondary,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ],
+                          ),
+                          Column(
+                            children: [
+                              Container(
+                                padding: const EdgeInsets.fromLTRB(10, 5, 10, 5),
+                                decoration: BoxDecoration(
+                                  color: Theme.of(context).colorScheme.surface,
+                                  borderRadius: BorderRadius.circular(10.0),
+                                  border: Border.all(
+                                    color:
+                                        Theme.of(context).colorScheme.secondary,
+                                  ),
+                                ),
+                                child: Column(
+                                  children: [
+                                    Text(
+                                      "20",
+                                      style: TextStyle(
+                                        fontWeight: FontWeight.w700,
+                                        fontFamily: "Inter",
+                                        fontSize: 20,
+                                        color: Theme.of(context)
+                                            .colorScheme
+                                            .secondary,
+                                      ),
+                                    ),
+                                    Text(
+                                      "Points",
+                                      style: TextStyle(
+                                        fontFamily: "Inter",
+                                        fontSize: 12,
+                                        color: Theme.of(context)
+                                            .colorScheme
+                                            .secondary,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                Divider(
+                  color: Theme.of(context).colorScheme.secondary,
+                  thickness: 0.3,
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(20, 0, 0, 0),
+                      child: Row(
+                        children: [
+                          const Text(
+                            style: TextStyle(
+                              fontFamily: "Inter",
+                            ),
+                            "Name",
+                          ),
+                          Icon(
+                            Icons.arrow_drop_down,
+                            color: Theme.of(context).colorScheme.primary,
+                          ),
+                        ],
+                      ),
+                    ),
+                    Row(
                       children: [
-                        //here the components will go for each user in the rankings
+                        const Text(
+                          style: TextStyle(
+                              fontFamily: "Inter",
+                            ),
+                          "Points",
+                        ),
+                        Icon(
+                          Icons.arrow_drop_down,
+                          color: Theme.of(context).colorScheme.primary,
+                        ),
                       ],
                     ),
-                  ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+          SingleChildScrollView(
+            scrollDirection: Axis.vertical,
+            child: Container(
+              decoration: BoxDecoration(
+                color: Theme.of(context).colorScheme.surface,
+              ),
+              child: Container(
+                padding: const EdgeInsets.fromLTRB(0, 10, 0, 0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Row(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.fromLTRB(0, 0, 5, 0),
+                          child: Text(
+                            "2nd",
+                            style: TextStyle(
+                              fontFamily: "Inter",
+                              fontSize: 12,
+                              color: Theme.of(context).colorScheme.secondary,
+                            ),
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.fromLTRB(5, 0, 0, 0),
+                          child: Container(
+                            decoration: BoxDecoration(
+                              color: const Color(0xFFD9D9D9),
+                              borderRadius: BorderRadius.circular(40),
+                            ),
+                            child: const SizedBox(
+                              width: 40,
+                              height: 40,
+                            ),
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.fromLTRB(10, 0, 0, 0),
+                          child: Text(
+                            "Team 2",
+                            style: TextStyle(
+                              fontFamily: "Inter",
+                              fontSize: 12,
+                              color: Theme.of(context).colorScheme.secondary,
+                            ),
+                          ),
+                        ),
+                        const Spacer(),
+                        Padding(
+                          padding: const EdgeInsets.fromLTRB(0, 0, 40, 0),
+                          child: Text(
+                            "17",
+                            style: TextStyle(
+                              fontFamily: "Inter",
+                              fontSize: 12,
+                              color: Theme.of(context).colorScheme.secondary,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
                 ),
               ),
-            ],
-          ),
-        ),
+            ),
+          )
+        ],
       ),
     );
   }
