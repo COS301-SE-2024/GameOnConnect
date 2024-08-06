2210ce40efd6f812131b6c576aa5df7d9e93629d
Shannon <131780205+shan-226@users.noreply.github.com>
2024-08-06 22:40:57 +0200

    💄 initial UI changes

--- lib/view/components/settings/edit_input_text.dart
+++ lib/view/components/settings/edit_input_text.dart
@@ -22,33 +22,35 @@ class _EditInputText extends State<EditInputText> {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(vertical: 8.0),
-      child: Row(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: <Widget>[
-          Expanded(
-            flex: 2,
-            child: Text(widget.label,
-                style:
-                    TextStyle(color: Theme.of(context).colorScheme.secondary)),
-          ),
-          Expanded(
-            flex: 4,
-            child: TextFormField(
-              key: widget.inputKey,
-              onTapOutside: (event) {
-                FocusManager.instance.primaryFocus?.unfocus();
-              },
-              onChanged: widget.onChanged,
-              decoration: InputDecoration(
-                enabledBorder: OutlineInputBorder(
-                  borderSide:
-                      BorderSide(color: Theme.of(context).colorScheme.primary),
-                  borderRadius: BorderRadius.circular(20.0),
-                ),
+          Text(widget.label,
+              style: TextStyle(color: Theme.of(context).colorScheme.secondary)),
+          const SizedBox(height: 5),
+          TextFormField(
+            key: widget.inputKey,
+            onTapOutside: (event) {
+              FocusManager.instance.primaryFocus?.unfocus();
+            },
+            onChanged: widget.onChanged,
+            decoration: InputDecoration(
+              filled: true,
+              fillColor: Theme.of(context).colorScheme.primaryContainer,
+              enabledBorder: OutlineInputBorder(
+                // borderSide: BorderSide(
+                //     color: Theme.of(context).colorScheme.primary),
+                borderRadius: BorderRadius.circular(10.0),
               ),
-              initialValue: widget.input,
-              style: TextStyle(color: Theme.of(context).colorScheme.secondary),
-              maxLines: widget.maxLines,
+              focusedBorder: OutlineInputBorder(
+                borderSide: BorderSide(
+                    color: Theme.of(context).colorScheme.primary),
+                borderRadius: BorderRadius.circular(10.0),
+              ),
             ),
+            initialValue: widget.input,
+            style: TextStyle(color: Theme.of(context).colorScheme.secondary),
+            maxLines: widget.maxLines,
           ),
         ],
       ),
