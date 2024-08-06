a0056dc1c739c857e904912b1bdfd375f247873b
Shannon <131780205+shan-226@users.noreply.github.com>
2024-08-06 22:37:42 +0200

    💄 initial UI changes

--- lib/view/components/settings/edit_switch.dart
+++ lib/view/components/settings/edit_switch.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-class EditSwitch extends StatefulWidget{
+class EditSwitch extends StatefulWidget {
   final bool currentValue;
   final String label;
   final void Function(bool value) onChanged;
@@ -15,7 +15,7 @@ class _EditSwitch extends State<EditSwitch> {
   late String label;
 
   @override
-  void initState(){
+  void initState() {
     super.initState();
     label = widget.label;
     currentValue = widget.currentValue;
@@ -35,34 +35,21 @@ class _EditSwitch extends State<EditSwitch> {
           Expanded(
             flex: 2,
             child: Text(widget.label,
-                style:
-                TextStyle(color: Theme
-                    .of(context)
-                    .colorScheme
-                    .secondary)),
+                style: TextStyle(color: Theme.of(context).colorScheme.secondary)),
           ),
           Expanded(
             flex: 4,
             child: Switch.adaptive(
-              value:currentValue,
+              value: currentValue,
               onChanged: (bool value) {
                 widget.onChanged(value);
                 setState(() {
                   currentValue = value;
                 });
               },
-              activeTrackColor: Theme
-                  .of(context)
-                  .colorScheme
-                  .primary,
-              inactiveTrackColor: Theme
-                  .of(context)
-                  .colorScheme
-                  .surface,
-              inactiveThumbColor: Theme
-                  .of(context)
-                  .colorScheme
-                  .secondary,
+              activeTrackColor: Theme.of(context).colorScheme.primary,
+              inactiveTrackColor: Theme.of(context).colorScheme.surface,
+              inactiveThumbColor: Theme.of(context).colorScheme.secondary,
             ),
           ),
         ],
@@ -70,3 +57,87 @@ class _EditSwitch extends State<EditSwitch> {
     );
   }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+// import 'package:flutter/material.dart';
+
+// class EditSwitch extends StatefulWidget{
+//   final bool currentValue;
+//   final String label;
+//   final void Function(bool value) onChanged;
+//   const EditSwitch({super.key, required this.currentValue, required this.label, required this.onChanged});
+
+//   @override
+//   State<EditSwitch> createState() => _EditSwitch();
+// }
+
+// class _EditSwitch extends State<EditSwitch> {
+//   late bool currentValue;
+//   late String label;
+
+//   @override
+//   void initState(){
+//     super.initState();
+//     label = widget.label;
+//     currentValue = widget.currentValue;
+//   }
+
+//   @override
+//   void dispose() {
+//     super.dispose();
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Padding(
+//       padding: const EdgeInsets.symmetric(vertical: 8.0),
+//       child: Row(
+//         children: <Widget>[
+//           Expanded(
+//             flex: 2,
+//             child: Text(widget.label,
+//                 style:
+//                 TextStyle(color: Theme
+//                     .of(context)
+//                     .colorScheme
+//                     .secondary)),
+//           ),
+//           Expanded(
+//             flex: 4,
+//             child: Switch.adaptive(
+//               value:currentValue,
+//               onChanged: (bool value) {
+//                 widget.onChanged(value);
+//                 setState(() {
+//                   currentValue = value;
+//                 });
+//               },
+//               activeTrackColor: Theme
+//                   .of(context)
+//                   .colorScheme
+//                   .primary,
+//               inactiveTrackColor: Theme
+//                   .of(context)
+//                   .colorScheme
+//                   .surface,
+//               inactiveThumbColor: Theme
+//                   .of(context)
+//                   .colorScheme
+//                   .secondary,
+//             ),
+//           ),
+//         ],
+//       ),
+//     );
+//   }
+// }
