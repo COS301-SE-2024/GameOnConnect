41f7640f976f9d916bbd71e6348bebfd5ad999b1
franco-dreyer <u22520016@tuks.co.za>
2024-08-09 02:01:55 +0200

    Added an animated splash screen page when logging in/signing up
    I experimented with a login/signup animation when a user logs into the up or creates a new account.

---
+++ lib/view/pages/feed/welcome_splash.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:gameonconnect/view/pages/feed/feed_page.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+
+    Future.delayed(const Duration(seconds: 4), () {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(
+            builder: (context) => const FeedPage(title: 'GameOnConnect')),
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Image.asset(
+              "assets/gifs/gaming_animation.gif",
+            ),
+            const Text(
+              'Welcome to',
+              style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+            ),
+            Text(
+              'GameOnConnect',
+              style: TextStyle(
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold,
+                  color: Theme.of(context).colorScheme.primary),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
