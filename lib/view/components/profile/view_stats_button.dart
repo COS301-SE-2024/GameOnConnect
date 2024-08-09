bfff676088354c09df7b8158ca16d1cbd6f592ce
Shannon <131780205+shan-226@users.noreply.github.com>
2024-08-09 23:31:04 +0200

    🚑 fixed naviagtion on new profile

--- lib/view/components/profile/view_stats_button.dart
+++ lib/view/components/profile/view_stats_button.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:gameonconnect/view/pages/stats/stats_page.dart';
 
 class StatsButton extends StatefulWidget {
 
@@ -16,35 +17,40 @@ class _StatsButtonState extends State<StatsButton> {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.fromLTRB(12, 10, 12, 10), // Adjust spacing as needed
-      child:Container(
-          height: 40,
-          decoration: BoxDecoration(
-            border: Border.all(color: Theme.of(context).colorScheme.primary),
-            borderRadius: BorderRadius.circular(10),
-          ),
-          child: Center(
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Icon(
-                Icons.bar_chart,
-                //size: 50,
-                color: Theme.of(context).colorScheme.primary,
-              ),
-
-              Text(
-                'View Stats',
-                style: TextStyle(
-                  fontWeight: FontWeight.w700,
-                  //fontSize: 12,
+      child: InkWell(
+        onTap: () {
+          Navigator.push(context, MaterialPageRoute(builder: (context) => const StatsPage()),
+          );
+        },
+        child: Container(
+            height: 40,
+            decoration: BoxDecoration(
+              border: Border.all(color: Theme.of(context).colorScheme.primary),
+              borderRadius: BorderRadius.circular(10),
+            ),
+            child: Center(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Icon(
+                  Icons.bar_chart,
+                  //size: 50,
                   color: Theme.of(context).colorScheme.primary,
                 ),
-              ),
-            ],
+                Text(
+                  'View Stats',
+                  style: TextStyle(
+                    fontWeight: FontWeight.w700,
+                    //fontSize: 12,
+                    color: Theme.of(context).colorScheme.primary,
+                  ),
+                ),
+              ],
+            ),
+            ),
+            
           ),
-          ),
-          
-        ),
+        )
     );
     
   }
