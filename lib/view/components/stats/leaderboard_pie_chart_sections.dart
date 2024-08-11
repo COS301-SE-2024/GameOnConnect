ed291cf9be64eef7f31c2ee4c3cd0a12dab2b08a
Shannon <131780205+shan-226@users.noreply.github.com>
2024-08-11 10:54:44 +0200

    💄 changed colors

--- lib/view/components/stats/leaderboard_pie_chart_sections.dart
+++ lib/view/components/stats/leaderboard_pie_chart_sections.dart
@@ -11,62 +11,62 @@ class LeaderboardPieChartSections {
       switch (i) {
         case 0:
           return PieChartSectionData(
-            color: const Color.fromRGBO(200, 235, 197, 1.0),
+            color: Theme.of(context).colorScheme.primary,     //const Color.fromRGBO(200, 235, 197, 1.0),
             value: leaderboardData['1st']!.toDouble(),
             title: '${leaderboardData['1st']}',
             radius: radius,
-            titleStyle: TextStyle(
+            titleStyle: const TextStyle(
               fontSize: fontSize,
               fontWeight: FontWeight.bold,
-              color: Theme.of(context).colorScheme.onSurface,
+              color:  Colors.black,
             ),
           );
         case 1:
           return PieChartSectionData(
-            color: const Color.fromRGBO(5, 94, 3, 1.0),
+            color: Theme.of(context).colorScheme.tertiary,   //const Color.fromRGBO(5, 94, 3, 1.0),
             value: leaderboardData['2nd']!.toDouble(),
             title: '${leaderboardData['2nd']}',
             radius: radius,
-            titleStyle: TextStyle(
+            titleStyle: const TextStyle(
               fontSize: fontSize,
               fontWeight: FontWeight.bold,
-              color: Theme.of(context).colorScheme.onSurface,
+              color:  Colors.black,
             ),
           );
         case 2:
           return PieChartSectionData(
-            color: const Color.fromRGBO(0, 182, 40, 1.0),
+            color: Theme.of(context).colorScheme.onPrimary,   //const Color.fromRGBO(0, 182, 40, 1.0),
             value: leaderboardData['3rd']!.toDouble(),
             title: '${leaderboardData['3rd']}',
             radius: radius,
-            titleStyle: TextStyle(
+            titleStyle: const TextStyle(
               fontSize: fontSize,
               fontWeight: FontWeight.bold,
-              color: Theme.of(context).colorScheme.onSurface,
+              color:  Colors.black,
             ),
           );
         case 3:
           return PieChartSectionData(
-            color: const Color.fromRGBO(0, 216, 35, 1.0),
+            color: Colors.black,       //const Color.fromRGBO(0, 216, 35, 1.0),
             value: leaderboardData['Top 5']!.toDouble(),
             title: '${leaderboardData['Top 5']}',
             radius: radius,
-            titleStyle: TextStyle(
+            titleStyle: const TextStyle(
               fontSize: fontSize,
               fontWeight: FontWeight.bold,
-              color: Theme.of(context).colorScheme.onSurface,
+              color:  Colors.white,
             ),
           );
         case 4:
           return PieChartSectionData(
-            color: const Color.fromRGBO(72, 228, 88, 1.0),
+            color: Colors.white,    //const Color.fromRGBO(72, 228, 88, 1.0),
             value: leaderboardData['Top 10']!.toDouble(),
             title: '${leaderboardData['Top 10']}',
             radius: radius,
-            titleStyle: TextStyle(
+            titleStyle: const TextStyle(
               fontSize: fontSize,
               fontWeight: FontWeight.bold,
-              color: Theme.of(context).colorScheme.onSurface,
+              color:  Colors.black,
             ),
           );
         default:
