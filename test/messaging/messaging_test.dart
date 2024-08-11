f868030636032aa2703112f6263ed0cd4b3f8a81
Pieryvdl <pieryvanderlinde@gmail.com>
2024-08-11 09:50:47 +0200

    moved test files into folders

--- test/messaging/messaging_test.dart
+++ test/messaging/messaging_test.dart
@@ -1,6 +1,6 @@
-import 'package:flutter/material.dart';
+//import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:gameonconnect/view/pages/messaging/messaging_page.dart';
+//import 'package:gameonconnect/view/pages/messaging/messaging_page.dart';
 
 void main() {
   group('Choose games Widget Tests', () {
