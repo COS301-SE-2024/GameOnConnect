1777f3ca377b011c4bb4fa86433de40f27c7a559
Pieryvdl <pieryvanderlinde@gmail.com>
2024-08-11 23:47:46 +0200

    firebase error

--- test/events/view_events_test.dart
+++ test/events/view_events_test.dart
@@ -12,6 +12,10 @@ void main() {
       //expect(find.byKey(const Key('history_icon_button')), findsOneWidget);
       //expect(find.text('All'), findsOneWidget);
       
+      //Struggling to resolve the firebase error
+      /* The following FirebaseException was thrown building ViewEvents(dirty, dependencies:
+        [_InheritedTheme, _LocalizationsScope-[GlobalKey#aceab]], state: _HomePageWidgetState#f2c6f):
+        [core/no-app] No Firebase App '[DEFAULT]' has been created - call Firebase.initializeApp() */
     });
   });
 }
