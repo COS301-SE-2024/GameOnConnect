dcb53ad62431d9afe9d231332c56c1aabba6aa6e
LeratoKgomo <162160922+LeratoKgomo@users.noreply.github.com>
2024-08-14 12:56:11 +0200

    created the tag container

---
+++ lib/view/components/settings/customize_tag_container.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+
+class TagContainer extends StatefulWidget {
+
+  const TagContainer({
+    super.key,
+    required this.tagType,
+    required this.onPressed,
+  });
+
+  final String tagType; // Either 'stats' or 'connect'
+  final VoidCallback onPressed;
+
+  @override
+  State<TagContainer> createState() => _TagContainerState();
+  
+  }
+
+
+class _TagContainerState extends State<TagContainer> {
+
+
+  @override
+  Widget build(BuildContext context) {
+  return Container(
+      height: 40,
+      margin: const EdgeInsets.fromLTRB(0, 25, 0, 0),
+      decoration: BoxDecoration(
+        border: Border.all(color: Theme.of(context).colorScheme.primary),
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child:  Padding(
+        padding: const EdgeInsets.fromLTRB(12, 0, 12, 0),
+        child:  Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Text(
+            widget.tagType,
+            style: const TextStyle(
+              fontWeight: FontWeight.w400,
+              fontSize: 12,
+              color: Color(0xFFBEBEBE), // dark green in dark mode
+            ),
+          ),
+          IconButton(
+            icon: const Icon(
+              Icons.arrow_drop_down,
+              color: Color(0xFFBEBEBE),
+            ),
+            onPressed: () {
+              widget.onPressed();
+            },
+          ),
+          
+        ],
+      ),
+      ),
+
+    );
+
+}
+}
