d488e132a3acf62dafb3ebd153cef74f9ba567b4
franco-dreyer <u22520016@tuks.co.za>
2024-08-20 17:46:21 +0200

    Added a fetchGames() to service
    I added a fetchGames() function to the service such that games can be fetched for the game library page

--- lib/services/game_library_S/game_service.dart
+++ lib/services/game_library_S/game_service.dart
@@ -1,11 +1,35 @@
 import 'dart:convert';
 import 'dart:io';
+import 'package:gameonconnect/model/game_library_M/game_model.dart';
+
 import '../../../globals.dart' as global;
 
 import '../../model/game_library_M/game_details_model.dart';
 import 'package:http/http.dart' as http;
 
 class GameService {
+  static Future<List<Game>> fetchGames(int page, {String? sortValue, String? searchQuery}) async {
+    String request = '&page_size=20&page=$page';
+    
+    if (sortValue != null && sortValue.isNotEmpty) {
+      request += '&ordering=-$sortValue';
+    }
+
+    if (searchQuery != null && searchQuery.isNotEmpty) {
+      request += '&search=$searchQuery';
+    }
+
+    final response = await http.get(Uri.parse('https://api.rawg.io/api/games?key=${global.apiKey}$request'));
+
+    if (response.statusCode == 200) {
+      final jsonData = jsonDecode(response.body);
+      return (jsonData['results'] as List)
+          .map((gameJson) => Game.fromJson(gameJson))
+          .toList();
+    } else {
+      throw Exception('Failed to load games');
+    }
+  }
 
   Future<GameDetails> fetchGameDetails( gameId) async {
     try {
