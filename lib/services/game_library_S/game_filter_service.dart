9a20d203b8b5b9e684f8d67863ae1bcb1e9ded22
franco-dreyer <u22520016@tuks.co.za>
2024-08-28 23:44:34 +0200

    Added a new filter service

---
+++ lib/services/game_library_S/game_filter_service.dart
@@ -0,0 +1,36 @@
+import 'dart:convert';
+
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:gameonconnect/model/game_library_M/game_filter_model.dart';
+import 'package:gameonconnect/model/game_library_M/game_filters_model.dart';
+
+class GameFilterService {
+  static get http => null;
+
+  static Future<FilterList> _createFilterList() async {
+    String? apikey = dotenv.env['RAWG_API_KEY'];
+
+    final storeFilters = await _fetchFilters('https://api.rawg.io/api/stores?key=$apikey');
+    final genreFilters = await _fetchFilters('https://api.rawg.io/api/genres?key=$apikey');
+    final tagFilters = await _fetchFilters('https://api.rawg.io/api/tags?key=$apikey');
+    final platformFilters = await _fetchFilters('https://api.rawg.io/api/platforms?key=$apikey');
+
+    return FilterList(
+      storeFilters: storeFilters,
+      genreFilters: genreFilters,
+      tagFilters: tagFilters,
+      platformFilters: platformFilters,
+    );
+  }
+
+  static Future<List<Filter>> _fetchFilters(String url) async {
+    final response = await http.get(Uri.parse(url));
+
+    if (response.statusCode == 200) {
+      final jsonData = jsonDecode(response.body);
+      return (jsonData['results'] as List).map((filterJson) => Filter.fromJson(filterJson)).toList();
+    } else {
+      throw Exception('Failed to load data from $url');
+    }
+  }
+}
