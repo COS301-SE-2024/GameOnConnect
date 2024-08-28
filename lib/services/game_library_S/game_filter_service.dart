1ebadedcaa9f276c627a788dbe88410c62afb53c
franco-dreyer <u22520016@tuks.co.za>
2024-08-29 00:25:14 +0200

    Added http package

--- lib/services/game_library_S/game_filter_service.dart
+++ lib/services/game_library_S/game_filter_service.dart
@@ -3,17 +3,20 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:gameonconnect/cache_managers/filtering_cache_manager.dart';
 import 'package:gameonconnect/model/game_library_M/game_filter_model.dart';
 import 'package:gameonconnect/model/game_library_M/game_filters_model.dart';
+import 'package:http/http.dart' as http;
 
 class GameFilterService {
-  static get http => null;
-
   static Future<FilterList> createFilterList() async {
     String? apikey = dotenv.env['RAWG_API_KEY'];
 
-    final storeFilters = await _fetchFilters('https://api.rawg.io/api/stores?key=$apikey');
-    final genreFilters = await _fetchFilters('https://api.rawg.io/api/genres?key=$apikey');
-    final tagFilters = await _fetchFilters('https://api.rawg.io/api/tags?key=$apikey');
-    final platformFilters = await _fetchFilters('https://api.rawg.io/api/platforms?key=$apikey');
+    final storeFilters =
+        await _fetchFilters('https://api.rawg.io/api/stores?key=$apikey');
+    final genreFilters =
+        await _fetchFilters('https://api.rawg.io/api/genres?key=$apikey');
+    final tagFilters =
+        await _fetchFilters('https://api.rawg.io/api/tags?key=$apikey');
+    final platformFilters =
+        await _fetchFilters('https://api.rawg.io/api/platforms?key=$apikey');
 
     return FilterList(
       storeFilters: storeFilters,
@@ -30,11 +33,9 @@ class GameFilterService {
       //Load the games from cache
       final jsonData = jsonDecode(await fileInfo.file.readAsString());
 
-      
-        return (jsonData['results'] as List)
-            .map((filterJson) => Filter.fromJson(filterJson))
-            .toList();
-      
+      return (jsonData['results'] as List)
+          .map((filterJson) => Filter.fromJson(filterJson))
+          .toList();
     } else {
       //Load the games from API
       final response = await http.get(Uri.parse(url));
@@ -47,11 +48,10 @@ class GameFilterService {
           fileExtension: 'json',
         );
         final jsonData = jsonDecode(response.body);
-     
-          return (jsonData['results'] as List)
-              .map((filterJson) => Filter.fromJson(filterJson))
-              .toList();
-       
+
+        return (jsonData['results'] as List)
+            .map((filterJson) => Filter.fromJson(filterJson))
+            .toList();
       } else {
         throw Exception('Failed to load tags');
       }
