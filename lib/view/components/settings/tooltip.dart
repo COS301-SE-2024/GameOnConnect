2f0129aa10b4a728e14f132b8c98dbc95aa204c7
Monika Theiss <monika.theiss03@gmail.com>
2024-08-30 14:39:19 +0200

    created tooltip component

--- lib/view/components/settings/tooltip.dart
+++ lib/view/components/settings/tooltip.dart
@@ -2,25 +2,26 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:super_tooltip/super_tooltip.dart';
 
-class ToolTip extends StatelessWidget{
+class ToolTip extends StatelessWidget {
   final String message;
 
   const ToolTip({super.key, required this.message});
 
-  Widget build(BuildContext context){
-    final tooltipkey =SuperTooltipController();
+  Widget build(BuildContext context) {
+    final tooltipkey = SuperTooltipController();
 
     return SuperTooltip(
-        controller: tooltipkey,
-        content: Text(
-         this.message,style:
-        TextStyle(color: Colors.black),
-          softWrap: true,),
-        verticalOffset: 13,
-        popupDirection: TooltipDirection.up,
-        backgroundColor: Colors.grey,
-        child:Container(child: const Icon(Icons.info)
-    ),
+      controller: tooltipkey,
+      content: Text(
+        message,
+        style: const TextStyle(color: Colors.black),
+        softWrap: true,
+      ),
+      verticalOffset: 13,
+      popupDirection: TooltipDirection.up,
+      backgroundColor: Colors.grey,
+      child: Container(
+          child: const Icon(Icons.info)),
     );
-}
+  }
 }
