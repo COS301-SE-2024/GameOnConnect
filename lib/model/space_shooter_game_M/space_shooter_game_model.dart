1d5ddbfd292cc6cc3a374dcaf7fb4ce679a05db5
Shannon <131780205+shan-226@users.noreply.github.com>
2024-09-04 23:07:37 +0200

    ✨ added bullets

--- lib/model/space_shooter_game_M/space_shooter_game_model.dart
+++ lib/model/space_shooter_game_M/space_shooter_game_model.dart
@@ -38,3 +38,38 @@ class SpaceShooterGame extends FlameGame with PanDetector {
     player.move(info.delta as Vector2);       //check this later
   }
 }
+
+class Bullet extends SpriteAnimationComponent
+    with HasGameReference<SpaceShooterGame> {
+  Bullet({
+    super.position,
+  }) : super(
+          size: Vector2(25, 50),
+          anchor: Anchor.center,
+        );
+
+  @override
+  Future<void> onLoad() async {
+    await super.onLoad();
+
+    animation = await game.loadSpriteAnimation(
+      'bullet.png',
+      SpriteAnimationData.sequenced(
+        amount: 4,
+        stepTime: .2,
+        textureSize: Vector2(8, 16),
+      ),
+    );
+  }
+
+  @override
+  void update(double dt) {
+    super.update(dt);
+
+    position.y += dt * -500;
+
+    if (position.y < -height) {
+      removeFromParent();
+    }
+  }
+}
--- lib/view/pages/space_shooter_game/space_shooter_game.dart
+++ lib/view/pages/space_shooter_game/space_shooter_game.dart
@@ -6,33 +6,6 @@ import 'package:flutter/material.dart';
 import 'package:flame/game.dart';
 import 'package:gameonconnect/model/space_shooter_game_M/space_shooter_game_model.dart';
 
-class Player extends SpriteAnimationComponent with HasGameReference<SpaceShooterGame> {
-  Player() : super(
-    size: Vector2(100, 150),
-    anchor: Anchor.center,
-  );
-
-  @override
-  Future<void> onLoad() async {
-    await super.onLoad();
-
-    animation = await game.loadSpriteAnimation(
-      'player.png',
-      SpriteAnimationData.sequenced(
-        amount: 4,
-        stepTime: .2,
-        textureSize: Vector2(32, 48),
-      ),
-    );
-
-    position = game.size / 2;
-  }
-
-  void move(Vector2 delta) {
-    position.add(delta);
-  }
-}
-
 class SpaceShooterGame extends FlameGame with PanDetector {
   late Player player;
 
@@ -54,16 +27,114 @@ class SpaceShooterGame extends FlameGame with PanDetector {
     add(player);
   }
 
-
   @override
   void onPanUpdate(DragUpdateInfo info) {
     player.move(info.delta.global);
+  }
+
+  @override
+  void onPanStart(DragStartInfo info) {
+    player.startShooting();
+  }
+
+  @override
+  void onPanEnd(DragEndInfo info) {
+    player.stopShooting();
   }
 }
 
 // void main() {
 //   runApp(GameWidget(game: SpaceShooterGame()));
 // }
+
+class Player extends SpriteAnimationComponent with HasGameReference<SpaceShooterGame> {
+  Player() : super(
+    size: Vector2(100, 150),
+    anchor: Anchor.center,
+  );
+
+  late final SpawnComponent _bulletSpawner;
+
+  @override
+  Future<void> onLoad() async {
+    await super.onLoad();
+
+    animation = await game.loadSpriteAnimation(
+      'player.png',
+      SpriteAnimationData.sequenced(
+        amount: 4,
+        stepTime: .2,
+        textureSize: Vector2(32, 48),
+      ),
+    );
+
+    position = game.size / 2;
+
+    _bulletSpawner = SpawnComponent(
+      period: .2,
+      selfPositioning: true,
+      factory: (index) {
+        return Bullet(
+          position: position +
+              Vector2(
+                0,
+                -height / 2,
+              ),
+        );
+      },
+      autoStart: false,
+    );
+
+    game.add(_bulletSpawner);
+  }
+
+  void move(Vector2 delta) {
+    position.add(delta);
+  }
+
+  void startShooting() {
+    _bulletSpawner.timer.start();
+  }
+
+  void stopShooting() {
+    _bulletSpawner.timer.stop();
+  }
+}
+
+class Bullet extends SpriteAnimationComponent
+    with HasGameReference<SpaceShooterGame> {
+  Bullet({
+    super.position,
+  }) : super(
+          size: Vector2(25, 50),
+          anchor: Anchor.center,
+        );
+
+  @override
+  Future<void> onLoad() async {
+    await super.onLoad();
+
+    animation = await game.loadSpriteAnimation(
+      'bullet.png',
+      SpriteAnimationData.sequenced(
+        amount: 4,
+        stepTime: .2,
+        textureSize: Vector2(8, 16),
+      ),
+    );
+  }
+
+  @override
+  void update(double dt) {
+    super.update(dt);
+
+    position.y += dt * -500;
+
+    if (position.y < -height) {
+      removeFromParent();
+    }
+  }
+}
 
 
 
