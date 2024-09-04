55706f720ba81c8765e37b45f582bfa2761ef761
Shannon <131780205+shan-226@users.noreply.github.com>
2024-09-04 22:57:40 +0200

    ✨ added animations to player

--- lib/view/pages/space_shooter_game/space_shooter_game.dart
+++ lib/view/pages/space_shooter_game/space_shooter_game.dart
@@ -1,11 +1,12 @@
 import 'package:flame/components.dart';
 import 'package:flame/events.dart';
 import 'package:flame/input.dart';
+import 'package:flame/parallax.dart';
 import 'package:flutter/material.dart';
 import 'package:flame/game.dart';
 import 'package:gameonconnect/model/space_shooter_game_M/space_shooter_game_model.dart';
 
-class Player extends SpriteComponent with HasGameRef<SpaceShooterGame> {
+class Player extends SpriteAnimationComponent with HasGameReference<SpaceShooterGame> {
   Player() : super(
     size: Vector2(100, 150),
     anchor: Anchor.center,
@@ -15,9 +16,16 @@ class Player extends SpriteComponent with HasGameRef<SpaceShooterGame> {
   Future<void> onLoad() async {
     await super.onLoad();
 
-    sprite = await gameRef.loadSprite('player-sprite.png');
+    animation = await game.loadSpriteAnimation(
+      'player.png',
+      SpriteAnimationData.sequenced(
+        amount: 4,
+        stepTime: .2,
+        textureSize: Vector2(32, 48),
+      ),
+    );
 
-    position = gameRef.size / 2;
+    position = game.size / 2;
   }
 
   void move(Vector2 delta) {
@@ -30,16 +38,26 @@ class SpaceShooterGame extends FlameGame with PanDetector {
 
   @override
   Future<void> onLoad() async {
-    await super.onLoad();
+    final parallax = await loadParallaxComponent(
+      [
+        ParallaxImageData('stars_0.png'),
+        ParallaxImageData('stars_1.png'),
+        ParallaxImageData('stars_2.png'),
+      ],
+      baseVelocity: Vector2(0, -5),
+      repeat: ImageRepeat.repeat,
+      velocityMultiplierDelta: Vector2(0, 5),
+    );
+    add(parallax);
 
     player = Player();
-
     add(player);
   }
 
+
   @override
   void onPanUpdate(DragUpdateInfo info) {
-    player.move(info.delta as Vector2);
+    player.move(info.delta.global);
   }
 }
 
