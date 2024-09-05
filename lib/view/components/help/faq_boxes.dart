caa1975e2a9e173cb69e5582c093890bfa226618
Monika Theiss <monika.theiss03@gmail.com>
2024-09-05 13:34:47 +0200

    added expansion tiles and styling

--- lib/view/components/help/faq_boxes.dart
+++ lib/view/components/help/faq_boxes.dart
@@ -3,7 +3,8 @@ import 'package:flutter/material.dart';
 class Faq_boxWidget extends StatefulWidget {
   final String question;
   final String answer;
-  const Faq_boxWidget({super.key, required this.answer, required this.question});
+  const Faq_boxWidget(
+      {super.key, required this.answer, required this.question});
 
   @override
   State<Faq_boxWidget> createState() => _Faq_boxWidgetState();
@@ -26,7 +27,6 @@ class _Faq_boxWidgetState extends State<Faq_boxWidget> {
     answer = widget.answer;
   }
 
-
   @override
   void dispose() {
     super.dispose();
@@ -34,47 +34,48 @@ class _Faq_boxWidgetState extends State<Faq_boxWidget> {
 
   @override
   Widget build(BuildContext context) {
-    return  Padding(
-        padding: const EdgeInsetsDirectional.fromSTEB(0, 0, 0, 1),
-        child: Container(
-          width: double.infinity,
-          color: Theme
-              .of(context)
-              .colorScheme
-              .primaryContainer,
-          child: Padding(
-            padding: const EdgeInsetsDirectional.fromSTEB(12, 12, 12, 12),
-            child: Row(
-              mainAxisSize: MainAxisSize.max,
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Flexible(child:
-                Text(
-                  question,
-                  style: TextStyle(
-                    color: Theme
-                        .of(context)
-                        .colorScheme
-                        .primary,
-                    fontSize: 16,
-                    fontWeight: FontWeight.normal,
+    return GestureDetector(
+        onTap: () {},
+        child: Padding(
+          padding: const EdgeInsetsDirectional.fromSTEB(0, 0, 0, 1),
+          child: Container(
+            width: double.infinity,
+            color: Theme.of(context).colorScheme.primaryContainer,
+            child: Padding(
+              padding: const EdgeInsetsDirectional.fromSTEB(12, 12, 12, 12),
+              child: Row(
+                mainAxisSize: MainAxisSize.max,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Flexible(
+                    child: Theme(data: Theme.of(context).copyWith(dividerColor: Theme.of(context).colorScheme.primaryContainer), child: ExpansionTile(
+                      title: Text(
+                        question,
+                        style: TextStyle(
+                          color: Theme.of(context).colorScheme.primary,
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      children: [
+                        Padding(padding: const EdgeInsets.all(13.0), child:Text(
+                          answer,
+                          
+                          style: TextStyle(
+                            color: Theme.of(context).colorScheme.secondary,
+                            fontSize: 16,
+                            fontWeight: FontWeight.normal,
+                          ),
+                        ),
+                        ),
+                      ],
+                    ),
+                    ),
                   ),
-
-                ),
-                ),
-                Icon(
-                  Icons.chevron_right_rounded,
-                  color: Theme
-                      .of(context)
-                      .colorScheme
-                      .primary,
-                  size: 24,
-                ),
-              ],
+                ],
+              ),
             ),
           ),
-        ),
-
-    );
+        ));
   }
 }
