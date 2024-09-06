fbae2c3599694b5a070fdb91841f2813e896e526
Pieryvdl <pieryvanderlinde@gmail.com>
2024-09-06 13:31:41 +0200

    added background to game

--- lib/GameyCon/components/levels.dart
+++ lib/GameyCon/components/levels.dart
@@ -2,10 +2,12 @@ import 'dart:async';
 
 import 'package:flame/components.dart';
 import 'package:flame_tiled/flame_tiled.dart';
+import 'package:gameonconnect/GameyCon/components/background_tile.dart';
 import 'package:gameonconnect/GameyCon/components/collision_block.dart';
 import 'package:gameonconnect/GameyCon/components/player.dart';
+import 'package:gameonconnect/GameyCon/gameycon_game.dart';
 
-class Level extends World {
+class Level extends World with HasGameRef<GameyCon> {
   final String levelName;
   final Player player;
   Level({required this.levelName, required this.player});
@@ -17,6 +19,38 @@ class Level extends World {
     level = await TiledComponent.load('$levelName.tmx', Vector2.all(16.0));
     add(level);
 
+    _scrollingBackground();
+    _spawningObjects();
+    _addCollisions();
+    return super.onLoad();
+  }
+
+  void _scrollingBackground() {
+    final backgroundLayer = level.tileMap.getLayer('Background');
+
+    const tileSize = 64;
+
+    final numTilesY = (game.size.y / tileSize).floor();
+    final numTilesX = (game.size.x / tileSize).floor();
+
+    if (backgroundLayer != null) {
+      final backgroundColor =
+          backgroundLayer.properties.getValue('BackgroundColor');
+
+      for (double y = 0; y < game.size.y / numTilesY; y++) {
+        for (double x = 0; x < numTilesX; x++) {
+          final backgroundTile = BackgroundTile(
+            color: backgroundColor ?? 'Gray',
+            position: Vector2(x * tileSize, y * tileSize - tileSize),
+          );
+
+          add(backgroundTile);
+        }
+      }
+    }
+  }
+
+  void _spawningObjects() {
     final spawnPointsLayer = level.tileMap.getLayer<ObjectGroup>('Spawnpoints');
 
     if (spawnPointsLayer != null) {
@@ -30,7 +64,9 @@ class Level extends World {
         }
       }
     }
+  }
 
+  void _addCollisions() {
     final collisionsLayer = level.tileMap.getLayer<ObjectGroup>('Collisions');
 
     if (collisionsLayer != null) {
@@ -46,17 +82,16 @@ class Level extends World {
             add(platform);
             break;
           default:
-          final block = CollisionBlock(
-            position: Vector2(collision.x, collision.y),
-            size: Vector2(collision.width, collision.height),
-          );
-          collisionBlocks.add(block);
-          add(block);
+            final block = CollisionBlock(
+              position: Vector2(collision.x, collision.y),
+              size: Vector2(collision.width, collision.height),
+            );
+            collisionBlocks.add(block);
+            add(block);
         }
       }
     }
 
     player.collisionBlocks = collisionBlocks;
-    return super.onLoad();
   }
 }
