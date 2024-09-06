963573051c864fe432ff39d9353cb5b0d73f9893
Monika Theiss <monika.theiss03@gmail.com>
2024-09-06 12:18:26 +0200

    Grouped FAQ's together, like on getting started page

--- lib/view/pages/settings/faq.dart
+++ lib/view/pages/settings/faq.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:gameonconnect/view/components/appbars/backbutton_appbar_component.dart';
 import 'package:gameonconnect/view/components/help/faq_boxes.dart';
-
 
 class FaqWidget extends StatefulWidget {
   const FaqWidget({super.key});
@@ -10,7 +10,6 @@ class FaqWidget extends StatefulWidget {
 }
 
 class _FaqWidgetState extends State<FaqWidget> {
-
   final scaffoldKey = GlobalKey<ScaffoldState>();
 
   @override
@@ -28,105 +27,129 @@ class _FaqWidgetState extends State<FaqWidget> {
     return GestureDetector(
       onTap: () => FocusScope.of(context).unfocus(),
       child: Scaffold(
+        appBar: BackButtonAppBar(
+          title: "FAQ's",
+          onBackButtonPressed: () {
+            Navigator.pop(context);
+          },
+          iconkey: const Key('Back_button_key'),
+          textkey: const Key('Help_text'),
+        ),
         key: scaffoldKey,
         backgroundColor: Theme.of(context).colorScheme.surface,
         body: SafeArea(
           top: true,
           child: Padding(
-            padding:const  EdgeInsetsDirectional.fromSTEB(12, 0, 12, 0),
+            padding: const EdgeInsetsDirectional.fromSTEB(12, 12, 12, 0),
             child: SingleChildScrollView(
               child: Column(
                 mainAxisSize: MainAxisSize.max,
                 mainAxisAlignment: MainAxisAlignment.start,
                 children: [
-                  Padding(
-                    padding: const EdgeInsetsDirectional.fromSTEB(12, 12, 12, 12),
-                    child: Text(
-                      'Frequently Asked Questions:',
-                      style: TextStyle(
-                        color: Theme.of(context).colorScheme.secondary,
-                        fontSize: 26,
-                      ),
+                  Container(
+                    margin: const EdgeInsets.all(10),
+                    decoration: BoxDecoration(
+                        color: Theme.of(context).colorScheme.primaryContainer,
+                        borderRadius: BorderRadius.circular(10)),
+                    child: const Column(
+                      children: [
+                        Text(
+                          "Account and connections",
+                          style: TextStyle(
+                            fontSize: 24,
+                          ),
+                        ),
+                        Faq_boxWidget(
+                          question: "What are connections?",
+                          answer:
+                              "Connections are any profiles on GameOnConnect "
+                              "who you choose to connect with. You can view their"
+                              " profiles and "
+                              "invite them specifically to any event you "
+                              "choose to create",
+                        ),
+                        Faq_boxWidget(
+                          question: "How are profiles made unique?",
+                          answer: "To make it easier for you to search for "
+                              "someone specific, we assign everyone a unique number. "
+                              "This way you can distinguish between all the different profiles"
+                              "and easily find your friends on GameOnConnect",
+                        ),
+                        Faq_boxWidget(
+                          question:
+                              "What is the difference between a private and "
+                              "public account?",
+                          answer:
+                              "If your account is private, anyone who is not "
+                              "your connection, will not be able to see any information"
+                              " when viewing your profile. If your account is public"
+                              " anyone will be able to view your profile, including"
+                              " your stats, My Games list, Want to play list as well as"
+                              " your connections list. ",
+                        ),
+                        Faq_boxWidget(
+                          question:
+                              "What is the difference between My Games and Want to play list?",
+                          answer:
+                              "Your My Games list is a list where you can add all the games you currently"
+                              " have or are currently playing. Your Want to play list "
+                              "is a list where you can add all the games you still "
+                              "want to play or are interested in",
+                        ),
+                      ],
                     ),
                   ),
-                   Divider(
-                    thickness: 1,
-                    color: Theme.of(context).colorScheme.primaryContainer,
-                  ),
-                  // add headings + dividers to faq
-                  const Faq_boxWidget(
-                    question:"What are connections?" ,
-                    answer: "Connections are any profiles on GameOnConnect "
-                        "who you choose to connect with. You can view their"
-                        " profiles and "
-                        "invite them specifically to any event you "
-                        "choose to create",
-                  ),
-                  const Faq_boxWidget(
-                    question:"How are profiles made unique?" ,
-                    answer: "To make it easier for you to search for "
-                        "someone specific, we assign everyone a unique number. "
-                        "This way you can distinguish between all the different profiles"
-                        "and easily find your friends on GameOnConnect",
-                  ),
-                  const Faq_boxWidget(
-                    question:"Can I play a game through GameOnConnect?" ,
-                    answer: "Unfortunately you cannot ply a game through "
-                        "GameOnConnect. GameOnConnect is a platform for gamers"
-                        " to get to know one another and to easily schedule "
-                        "sessions. You are also able to time your gaming "
-                        "activity to update your gaming stats for all your "
-                        "connections to see. ",
-                  ),
-                  const Faq_boxWidget(
-                    question:"What is the difference between gaming sessions"
-                        " and tournaments?" ,
-                    answer: "When creating an event, you can choose to make"
-                        " the event a gaming session or a tournament. Tournaments"
-                        " are competitions, and so there is a lot of pressure"
-                        " to perform. Gaming sessions on the other hand "
-                        "are more relaxed events, where you can spend time "
-                        "playing a game with your connections.",
-                  ),
-                  const Faq_boxWidget(
-                    question:"What is the difference between a private and "
-                        "public account?" ,
-                    answer: "If your account is private, anyone who is not "
-                        "your connection, will not be able to see any information"
-                        " when viewing your profile. If your account is public"
-                        " anyone will be able to view your profile, including"
-                        " your stats, My Games list, Want to play list as well as"
-                        " your connections list. ",
-                  ),
-                  const Faq_boxWidget(
-                    question:"What is the difference between a private and "
-                        "public event?" ,
-                    answer: "Your event is set to private by default. This means"
-                        "only the connections you have invited are able to join"
-                        " the event. If you decide to make your event public, "
-                        "any user on GameOnConnect will be able to join and"
-                        " partake in your event.",
-                  ),
-                  const Faq_boxWidget(
-                    question:"What is the difference between rating and score?" ,
-                    answer: "Honestly I do not know either :)",
-                  ),
-                  const Faq_boxWidget(
-                    question:"What is the difference between metacritic and rating?" ,
-                    answer: "I  cannot say until I know what rating is"
-                        " and whether we are removing it or not",
-                  ),
-                  const Faq_boxWidget(
-                    question:"What is the difference between My Games and Want to play list?" ,
-                    answer: "Your My Games list is a list where you can add all the games you currently"
-                        " have or are currently playing. Your Want to play list "
-                        "is a list where you can add all the games you still "
-                        "want to play or are interested in",
-                  ),
-
-
-
-
+                  Container(
+                      margin: const EdgeInsets.all(10),
+                      padding: EdgeInsets.only(top: 24),
+                      decoration: BoxDecoration(
+                          color: Theme.of(context).colorScheme.primaryContainer,
+                          borderRadius: BorderRadius.circular(10)),
+                      child: const Column(
+                        children: [
+                          Text(
+                            "Games and events",
+                            style: TextStyle(
+                              fontSize: 24,
+                            ),
+                          ),
+                          Faq_boxWidget(
+                            question:
+                                "Can I play a game through GameOnConnect?",
+                            answer:
+                                "Unfortunately you cannot ply a game through "
+                                "GameOnConnect. GameOnConnect is a platform for gamers"
+                                " to get to know one another and to easily schedule "
+                                "sessions. You are also able to time your gaming "
+                                "activity to update your gaming stats for all your "
+                                "connections to see. ",
+                          ),
+                          Faq_boxWidget(
+                            question:
+                                "What is the difference between gaming sessions"
+                                " and tournaments?",
+                            answer:
+                                "When creating an event, you can choose to make"
+                                " the event a gaming session or a tournament. Tournaments"
+                                " are competitions, and so there is a lot of pressure"
+                                " to perform. Gaming sessions on the other hand "
+                                "are more relaxed events, where you can spend time "
+                                "playing a game with your connections.",
+                          ),
+                          Faq_boxWidget(
+                            question:
+                                "What is the difference between a private and "
+                                "public event?",
+                            answer:
+                                "Your event is set to private by default. This means"
+                                "only the connections you have invited are able to join"
+                                " the event. If you decide to make your event public, "
+                                "any user on GameOnConnect will be able to join and"
+                                " partake in your event.",
+                          ),
+                          // Account & connections
+                        ],
+                      ))
                 ],
               ),
             ),
--- lib/view/pages/settings/getting_started.dart
+++ lib/view/pages/settings/getting_started.dart
@@ -1,4 +1,3 @@
-// ignore_for_file: prefer_const_constructors
 import 'package:flutter/material.dart';
 import 'package:gameonconnect/view/components/appbars/backbutton_appbar_component.dart';
 import 'package:gameonconnect/view/components/help/all_faqs.dart';
@@ -15,13 +14,13 @@ class GettingStarted extends StatelessWidget {
           onBackButtonPressed: () {
             Navigator.pop(context);
           },
-          iconkey: Key('Back_button_key'),
-          textkey: Key('Help_text'),
+          iconkey: const Key('Back_button_key'),
+          textkey: const Key('Help_text'),
         ),
         body: Column(
           children: [
-            Padding(
-              padding: const EdgeInsets.all(25),
+            const Padding(
+              padding: EdgeInsets.all(25),
               child: Text(
                 "Quick answers to questions you may have. Can't find what your looking for? Contact us through our email address.",
                 style: TextStyle(
@@ -34,55 +33,6 @@ class GettingStarted extends StatelessWidget {
             AllFaqs()
           ],
         ),
-        /*GestureDetector(
-            onTap: () {
-              Navigator.pushNamed(context, '/profile_management');
-            },
-            child: Container(
-              height: 50,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(60),
-                color: Color.fromARGB(255, 128, 216, 50),
-              ),
-              child: const Center(child: Text("Profile Management")),
-            ),
-          ),
-          SizedBox(
-            height: 5,
-          ),
-          Container(
-            height: 50,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(60),
-              color: Color.fromARGB(255, 128, 216, 50),
-            ),
-            child: const Center(child: Text("Game Management")),
-          ),
-          SizedBox(
-            height: 5,
-          ),
-          Container(
-            height: 50,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(60),
-              color: Color.fromARGB(255, 128, 216, 50),
-            ),
-            child: const Center(child: Text("Social Management")),
-          ),
-          SizedBox(
-            height: 5,
-          ),
-          Container(
-            height: 50,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(60),
-              color: Color.fromARGB(255, 128, 216, 50),
-            ),
-            child: const Center(child: Text("Troubleshooting")),
-          ),
-          SizedBox(
-            height: 5,
-          ),*/
-        bottomNavigationBar: ContactUsBar());
+        bottomNavigationBar: const ContactUsBar());
   }
 }
