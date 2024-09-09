8e2e7b33411fe8b445aafd06170495127b7e924b
Shannon <131780205+shan-226@users.noreply.github.com>
2024-09-09 19:50:16 +0200

    ✨ added player explosions and game over screen

--- lib/view/pages/space_shooter_game/space_shooter_game.dart
+++ lib/view/pages/space_shooter_game/space_shooter_game.dart
@@ -7,16 +7,45 @@ import 'package:flame/input.dart';
 import 'package:flame/parallax.dart';
 import 'package:flutter/material.dart';
 
-// void main() {
-//   runApp(GameWidget(game: SpaceShooterGame()));
-// }
-
 class SpaceShooterGame extends FlameGame
     with PanDetector, HasCollisionDetection {
   late Player player;
+  bool isGameOver = false;
 
   @override
   Future<void> onLoad() async {
+    await initializeGame();
+
+    // final parallax = await loadParallaxComponent(
+    //   [
+    //     ParallaxImageData('stars_2.png'),
+    //     ParallaxImageData('stars_1.png'),
+    //     ParallaxImageData('stars_0.png'),
+    //   ],
+    //   baseVelocity: Vector2(0, -5),
+    //   repeat: ImageRepeat.repeat,
+    //   velocityMultiplierDelta: Vector2(0, 5),
+    // );
+    // add(parallax);
+
+    // player = Player();
+    // add(player);
+
+    // add(
+    //   SpawnComponent(
+    //     factory: (index) {
+    //       return Enemy();
+    //     },
+    //     period: 1,
+    //     area: Rectangle.fromLTWH(0, 0, size.x, -Enemy.enemySize),
+    //   ),
+    // );
+
+    overlays.add('quitButton');
+  }
+
+  Future<void> initializeGame() async {
+    // Load parallax background
     final parallax = await loadParallaxComponent(
       [
         ParallaxImageData('stars_2.png'),
@@ -29,9 +58,11 @@ class SpaceShooterGame extends FlameGame
     );
     add(parallax);
 
+    // Add player to the game
     player = Player();
     add(player);
 
+    // Spawn enemies
     add(
       SpawnComponent(
         factory: (index) {
@@ -57,13 +88,26 @@ class SpaceShooterGame extends FlameGame
   void onPanEnd(DragEndInfo info) {
     player.stopShooting();
   }
+
+  void triggerGameOver() {
+    isGameOver = true; // Set the game as over
+    overlays.add('gameOver'); // Show the game over overlay
+  }
+
+  void reset() {
+    // Clear all current components (e.g., enemies, bullets)
+    removeAll(children);
+
+    // Reinitialize the game
+    initializeGame();
+  }
 }
 
 class Player extends SpriteAnimationComponent
-    with HasGameReference<SpaceShooterGame> {
+    with HasGameReference<SpaceShooterGame>, CollisionCallbacks {
   Player()
       : super(
-          size: Vector2(120, 130),     //size: Vector2(120, 130), frigate
+          size: Vector2(110, 120),     //size: Vector2(120, 130), frigate
           anchor: Anchor.center,
         );
 
@@ -110,6 +154,11 @@ class Player extends SpriteAnimationComponent
     );
 
     game.add(_bulletSpawner);
+
+    add(RectangleHitbox.relative(
+      Vector2(0.3, 0.3), 
+      parentSize: size,
+    ));
   }
 
   void move(Vector2 delta) {
@@ -122,6 +171,22 @@ class Player extends SpriteAnimationComponent
 
   void stopShooting() {
     _bulletSpawner.timer.stop();
+  }
+
+  @override
+  void onCollisionStart(
+    Set<Vector2> intersectionPoints,
+    PositionComponent other,
+  ) {
+    super.onCollisionStart(intersectionPoints, other);
+
+    if (other is Enemy) {
+      removeFromParent();
+      other.removeFromParent();
+      game.add(PlayerExplosion(position: position));
+
+      game.overlays.add('gameOver');
+    }
   }
 }
 
@@ -141,17 +206,23 @@ class Bullet extends SpriteAnimationComponent
     animation = await game.loadSpriteAnimation(
       'bullet.png',
       SpriteAnimationData.sequenced(
-        amount: 4,
+        amount: 2,
         stepTime: .2,
         textureSize: Vector2(8, 8),
       ),
     );
 
-    add(
-      RectangleHitbox(
-        collisionType: CollisionType.passive,
-      ),
-    );
+    // add(
+    //   RectangleHitbox(
+    //     collisionType: CollisionType.passive,
+    //   ),
+    // );
+    add(RectangleHitbox.relative(
+      Vector2(0.5, 0.5), 
+      parentSize: size,
+      collisionType: CollisionType.passive,
+    ));
+
   }
 
   @override
@@ -190,7 +261,11 @@ class Enemy extends SpriteAnimationComponent
       ),
     );
 
-    add(RectangleHitbox());
+    add(RectangleHitbox.relative(
+      Vector2(0.5, 0.5), 
+      parentSize: size,
+    ));
+
   }
 
   @override
@@ -214,14 +289,14 @@ class Enemy extends SpriteAnimationComponent
     if (other is Bullet) {
       removeFromParent();
       other.removeFromParent();
-      game.add(Explosion(position: position));
+      game.add(EnemyExplosion(position: position));
     }
   }
 }
 
-class Explosion extends SpriteAnimationComponent
+class EnemyExplosion extends SpriteAnimationComponent
     with HasGameReference<SpaceShooterGame> {
-  Explosion({
+  EnemyExplosion({
     super.position,
   }) : super(
           size: Vector2.all(100),
@@ -245,249 +320,28 @@ class Explosion extends SpriteAnimationComponent
   }
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// import 'package:flame/components.dart';
-// import 'package:flame/events.dart';
-// import 'package:flame/experimental.dart';
-// import 'package:flame/input.dart';
-// import 'package:flame/parallax.dart';
-// import 'package:flutter/material.dart';
-// import 'package:flame/game.dart';
-// import 'package:gameonconnect/model/space_shooter_game_M/space_shooter_game_model.dart';
-
-// class Player extends SpriteAnimationComponent with HasGameReference<SpaceShooterGame> {
-//   Player() : super(
-//     size: Vector2(100, 150),
-//     anchor: Anchor.center,
-//   );
-
-//   late final SpawnComponent _bulletSpawner;
-
-//   @override
-//   Future<void> onLoad() async {
-//     await super.onLoad();
-
-//     animation = await game.loadSpriteAnimation(
-//       'player.png',
-//       SpriteAnimationData.sequenced(
-//         amount: 4,
-//         stepTime: .2,
-//         textureSize: Vector2(32, 48),
-//       ),
-//     );
-
-//     position = game.size / 2;
-
-//     _bulletSpawner = SpawnComponent(
-//       period: .2,
-//       selfPositioning: true,
-//       factory: (index) {
-//         return Bullet(
-//           position: position +
-//               Vector2(
-//                 0,
-//                 -height / 2,
-//               ),
-//         );
-//       },
-//       autoStart: false,
-//     );
-
-//     game.add(_bulletSpawner);
-//   }
-
-//   void move(Vector2 delta) {
-//     position.add(delta);
-//   }
-
-//   void startShooting() {
-//     _bulletSpawner.timer.start();
-//   }
-
-//   void stopShooting() {
-//     _bulletSpawner.timer.stop();
-//   }
-// }
-
-// class Bullet extends SpriteAnimationComponent
-//     with HasGameReference<SpaceShooterGame> {
-//   Bullet({
-//     super.position,
-//   }) : super(
-//           size: Vector2(25, 50),
-//           anchor: Anchor.center,
-//         );
-
-//   @override
-//   Future<void> onLoad() async {
-//     await super.onLoad();
-
-//     animation = await game.loadSpriteAnimation(
-//       'bullet.png',
-//       SpriteAnimationData.sequenced(
-//         amount: 4,
-//         stepTime: .2,
-//         textureSize: Vector2(8, 16),
-//       ),
-//     );
-//   }
-
-//   @override
-//   void update(double dt) {
-//     super.update(dt);
-
-//     position.y += dt * -500;
-
-//     if (position.y < -height) {
-//       removeFromParent();
-//     }
-//   }
-// }
-
-// class Enemy extends SpriteAnimationComponent
-//     with HasGameReference<SpaceShooterGame> {
-
-//   Enemy({
-//     super.position,
-//   }) : super(
-//           size: Vector2.all(enemySize),
-//           anchor: Anchor.center,
-//         );
-
-
-//   static const enemySize = 50.0;
-
-//   @override
-//   Future<void> onLoad() async {
-//     await super.onLoad();
-
-//     animation = await game.loadSpriteAnimation(
-//       'enemy.png',
-//       SpriteAnimationData.sequenced(
-//         amount: 4,
-//         stepTime: .2,
-//         textureSize: Vector2.all(16),
-//       ),
-//     );
-//   }
-
-//   @override
-//   void update(double dt) {
-//     super.update(dt);
-
-//     position.y += dt * 250;
-
-//     if (position.y > game.size.y) {
-//       removeFromParent();
-//     }
-//   }
-// }
-
-// class SpaceShooterGame extends FlameGame with PanDetector {
-//   late Player player;
-
-//   @override
-//   Future<void> onLoad() async {
-//     final parallax = await loadParallaxComponent(
-//       [
-//         ParallaxImageData('stars_0.jpg'),
-//         ParallaxImageData('stars_0.jpg'),
-//         ParallaxImageData('stars_0.jpg'),
-//       ],
-//       baseVelocity: Vector2(0, -5),
-//       repeat: ImageRepeat.repeat,
-//       velocityMultiplierDelta: Vector2(0, 5),
-//     );
-//     add(parallax);
-
-//     player = Player();
-//     add(player);
-
-//     add(
-//       SpawnComponent(
-//         factory: (index) {
-//           return Enemy();
-//         },
-//         period: 1,
-//         area: Rectangle.fromLTWH(0, 0, size.x, -Enemy.enemySize),
-//       ),
-//     );
-//   }
-
-//   @override
-//   void onPanUpdate(DragUpdateInfo info) {
-//     player.move(info.delta.global);
-//   }
-
-//   @override
-//   void onPanStart(DragStartInfo info) {
-//     player.startShooting();
-//   }
-
-//   @override
-//   void onPanEnd(DragEndInfo info) {
-//     player.stopShooting();
-//   }
-// }
-
-// // void main() {
-// //   runApp(GameWidget(game: SpaceShooterGame()));
-// // }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// import 'package:flutter/material.dart';
-// import 'package:flame/game.dart';
-// import 'package:gameonconnect/model/space_shooter_game_M/space_shooter_game_model.dart';
-
-// class SpaceShooterPage extends StatelessWidget {
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: Text('Space Shooter'),
-//       ),
-//       body: GameWidget(
-//         game: SpaceShooterGame(),
-//       ),
-//     );
-//   }
-// }
+class PlayerExplosion extends SpriteAnimationComponent
+    with HasGameReference<SpaceShooterGame> {
+  PlayerExplosion({
+    super.position,
+  }) : super(
+          size: Vector2(120, 130),
+          anchor: Anchor.center,
+          removeOnFinish: true,
+        );
+
+  @override
+  Future<void> onLoad() async {
+    await super.onLoad();
+
+    animation = await game.loadSpriteAnimation(
+      'Frigate_Destruction.png',
+      SpriteAnimationData.sequenced(
+        amount: 9,
+        stepTime: .2,
+        textureSize: Vector2.all(64),
+        loop: false,
+      ),
+    );
+  }
+}
