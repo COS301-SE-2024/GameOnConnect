41d27fe33ee88395d815cb9f26eec8880b88357e
LeratoKgomo <162160922+LeratoKgomo@users.noreply.github.com>
2024-09-11 09:06:49 +0200

    created start game screen

---
+++ lib/view/pages/flappy_bird/start_game_screen.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:gameonconnect/view/pages/flappy_bird/flappy_bird.dart';
+
+class StartScreen extends StatelessWidget {
+  
+  const StartScreen({
+    Key? key,
+    required this.game,
+  }) : super(key: key);
+
+  final FlappyBird game;
+  static const String id = 'start';
+
+  @override
+  Widget build(BuildContext context) {
+    game.pauseEngine();
+
+    return Scaffold(
+      body:GestureDetector(
+        onTap: () {
+          game.overlays.remove('start');
+          game.resumeEngine();
+        },
+        child: Container(
+          width: double.infinity,
+          height: double.infinity,
+          decoration: const BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage('assets/images/menu.jpg'),
+              fit: BoxFit.cover,
+            ),
+          ),
+          child: Image.asset('assets/images/message.png'),
+        ),
+      ),
+    );
+  }
+}
