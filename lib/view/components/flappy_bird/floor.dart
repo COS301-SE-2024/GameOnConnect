892dc00d3a722e60f26e225f1739d90c666925f9
LeratoKgomo <162160922+LeratoKgomo@users.noreply.github.com>
2024-09-11 03:13:11 +0200

    added hitbox and speed update

--- lib/view/components/flappy_bird/floor.dart
+++ lib/view/components/flappy_bird/floor.dart
@@ -1,47 +1,29 @@
+import 'package:flame/collisions.dart';
 import 'package:flame/components.dart';
 import 'package:flame/flame.dart';
 import 'package:flame/parallax.dart';
 import 'package:gameonconnect/view/pages/flappy_bird/flappy_bird.dart';
 
-class Floor extends ParallaxComponent<FlappyBird>{
+class Floor extends ParallaxComponent<FlappyBird>with HasGameRef<FlappyBird>{
   Floor();
   final double speed = 200;
-  final double floorHeight = 100;
+  final double floorHeight = 110;
 
   @override
   Future<void> onLoad() async{
     final floor = await Flame.images.load('ground.png');
      parallax = Parallax([
       ParallaxLayer(
-        ParallaxImage(floor, fill: LayerFill.none),
+        ParallaxImage(
+          floor, fill: LayerFill.none),
       ),
     ]);
-  }
 
-  @override
-  void update(double dt) {
-    super.update(dt);
-    parallax?.baseVelocity.x = this.speed;
-  }
-}
-
-/**
- * class Ground extends ParallaxComponent<FlappyBirdGame>
-    with HasGameRef<FlappyBirdGame> {
-  Ground();
-
-  @override
-  Future<void> onLoad() async {
-    final ground = await Flame.images.load(Assets.ground);
-    parallax = Parallax([
-      ParallaxLayer(
-        ParallaxImage(ground, fill: LayerFill.none),
-      ),
-    ]);
     add(
+      //box arround object
       RectangleHitbox(
-        position: Vector2(0, gameRef.size.y - Config.groundHeight),
-        size: Vector2(gameRef.size.x, Config.groundHeight),
+        position: Vector2(0, gameRef.size.y- floorHeight),
+        size: Vector2(gameRef.size.x, floorHeight),
       ),
     );
   }
@@ -49,7 +31,7 @@ class Floor extends ParallaxComponent<FlappyBird>{
   @override
   void update(double dt) {
     super.update(dt);
-    parallax?.baseVelocity.x = Config.gameSpeed;
+    parallax?.baseVelocity.x = speed;
   }
 }
- */
+
