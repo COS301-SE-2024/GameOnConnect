af60a2596c9676ea894bab09ed9b91c388fd4755
franco-dreyer <u22520016@tuks.co.za>
2024-09-12 14:13:10 +0200

    Updated the badge page to show assets

--- lib/view/pages/badges/achievement_badges.dart
+++ lib/view/pages/badges/achievement_badges.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 
 class AchievementBadgesPage extends StatefulWidget {
   const AchievementBadgesPage({super.key});
@@ -12,15 +11,28 @@ class _AchievementBadgesPageState extends State<AchievementBadgesPage> {
   //this is temporary
   List<String> badges = [
     'Loyalty',
-    'Mountain',
-    'Social Butterfly',
-    'Customizer',
-    'Newbie',
     'Gamer',
-    'Collector',
-    'Messanger',
     'Night Owl',
-    'Globetrotter'
+    'Newbie',
+    'Explorer',
+    'Event Planner',
+    'Customizer',
+    'Collector',
+    'Achiever',
+    'Social Butterfly'
+  ];
+
+  List<String> badgeImages = [
+    'loyalty_badge.png',
+    'gamer_badge.png',
+    'night_owl_badge.png',
+    'newbie_badge.png',
+    'explorer_badge.png',
+    'event_planner_badge.png',
+    'customizer_badge.png',
+    'collector_badge.png',
+    'achiever_badge.png',
+    'social_butterfly_badge.png'
   ];
 
   @override
@@ -29,44 +41,55 @@ class _AchievementBadgesPageState extends State<AchievementBadgesPage> {
       appBar: AppBar(
         title: const Text('My Badges'),
       ),
-      body: GridView.builder(
-        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 2,
-          crossAxisSpacing: 20,
-          mainAxisSpacing: 20,
-        ),
-        itemCount: badges.length,
-        itemBuilder: (context, index) {
-          return Container(
-            decoration: BoxDecoration(
-              color: Theme.of(context).colorScheme.primaryContainer,
-              borderRadius: BorderRadius.circular(15),
-            ),
-            child: Center(
-              child: Column(
-                children: [
-                  Expanded(
-                      child: Image.network('https://developers.elementor.com/docs/assets/img/elementor-placeholder-image.png',
-                          fit: BoxFit.cover)),
-                  Padding(
-                    padding: const EdgeInsets.all(9),
-                    child: Text(
-                      badges[index],
-                      style: const TextStyle(color: Colors.white, fontSize: 14, fontWeight: FontWeight.bold),
-                    ),
+      body: Padding(
+        padding: const EdgeInsets.all(12),
+        child: GridView.builder(
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+            crossAxisSpacing: 20,
+            mainAxisSpacing: 20,
+          ),
+          itemCount: badges.length,
+          itemBuilder: (context, index) {
+            return GestureDetector(
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Theme.of(context).colorScheme.primaryContainer,
+                  borderRadius: BorderRadius.circular(15),
+                ),
+                child: Center(
+                  child: Column(
+                    children: [
+                      Expanded(
+                          child: Image.asset(
+                              'assets/badges_images/${badgeImages[index]}',
+                              height: 85,
+                              width: 85,
+                              fit: BoxFit.contain)),
+                      Padding(
+                        padding: const EdgeInsets.only(top: 9),
+                        child: Text(
+                          "${badges[index]} Badge",
+                          style: const TextStyle(
+                              color: Colors.white,
+                              fontSize: 14,
+                              fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                      const Padding(
+                        padding: EdgeInsets.all(9),
+                        child: Text(
+                          "2024/01/02",
+                          style: TextStyle(color: Colors.grey, fontSize: 12),
+                        ),
+                      ),
+                    ],
                   ),
-                  const Padding(
-                    padding: EdgeInsets.all(9),
-                    child: Text(
-                      "2024/01/02",
-                      style: TextStyle(color: Colors.grey, fontSize: 12),
-                    ),
-                  ),
-                ],
+                ),
               ),
-            ),
-          );
-        },
+            );
+          },
+        ),
       ),
     );
   }
