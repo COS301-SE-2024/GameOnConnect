c28ecc60bc64b557efb90a2958ea8738b41cdc50
franco-dreyer <u22520016@tuks.co.za>
2024-09-24 20:39:58 +0200

    Added a badge model

---
+++ lib/model/badges_M/badge_model.dart
@@ -0,0 +1,7 @@
+class Badge {
+  final String badgeName;
+  final String badgeFile;
+  final String badgeDescription;
+
+  Badge({required this.badgeName, required this.badgeFile, required this.badgeDescription});
+}
