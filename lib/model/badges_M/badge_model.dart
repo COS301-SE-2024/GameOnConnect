3655d6fe4511e166a6cf15cfdee0d79058f1d6e6
franco-dreyer <u22520016@tuks.co.za>
2024-09-24 22:55:42 +0200

    Removed model and used database data for badges

--- lib/model/badges_M/badge_model.dart
+++
@@ -1,9 +0,0 @@
-class BadgeModel {
-  final String badgeName;
-  final String badgeFile;
-  final String badgeDescription;
-  bool unlocked = false;
-  DateTime dateUnlocked = DateTime.now();
-
-  BadgeModel({required this.badgeName, required this.badgeFile, required this.badgeDescription});
-}
