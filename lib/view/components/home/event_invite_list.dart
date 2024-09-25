e16294483668da4397d5d7bdbfd76b4c851f3974
franco-dreyer <u22520016@tuks.co.za>
2024-09-25 17:05:30 +0200

    Updates the event invite list to be a whole page

--- lib/view/components/home/event_invite_list.dart
+++ lib/view/components/home/event_invite_list.dart
@@ -3,6 +3,7 @@ import 'package:flutter_svg/svg.dart';
 import 'package:gameonconnect/services/events_S/event_service.dart';
 import 'package:gameonconnect/model/events_M/events_model.dart';
 import 'package:gameonconnect/view/components/home/event_invite.dart';
+import 'package:loading_animation_widget/loading_animation_widget.dart';
 
 class EventInvitesList extends StatefulWidget {
   const EventInvitesList({super.key});
@@ -14,6 +15,7 @@ class EventInvitesList extends StatefulWidget {
 class _EventInvitesListState extends State<EventInvitesList> {
   final EventsService _eventsService = EventsService();
   List<Event> _invitedEvents = [];
+  bool _isLoading = true; // Added loading state
 
   @override
   void initState() {
@@ -28,43 +30,64 @@ class _EventInvitesListState extends State<EventInvitesList> {
     setState(() {
       _invitedEvents =
           invitedEvents.map((e) => Event.fromMap(e, e['id'])).toList();
+      _isLoading = false;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    return _invitedEvents.isEmpty
-        ? Center(
-          child: Padding(
-            padding: const EdgeInsets.all(12),
-            child: Container(
-                height: 100,
-                padding: const EdgeInsets.all(20),
-                decoration: BoxDecoration(
-                  color: Theme.of(context).colorScheme.onPrimary.withOpacity(0.3),
-                  borderRadius: BorderRadius.circular(10),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Event Invites',
+          style: TextStyle(
+              fontSize: 15,
+              fontWeight: FontWeight.bold,
+              color: Theme.of(context).colorScheme.primary),
+        ),
+        iconTheme: IconThemeData(
+          color: Theme.of(context).colorScheme.primary,
+        ),
+      ),
+      body: _isLoading
+          ? Center(
+              child: LoadingAnimationWidget.halfTriangleDot(
+                color: Theme.of(context).colorScheme.primary,
+                size: 36,
+              ),
+            )
+          : _invitedEvents.isEmpty
+              ? Container(
+                  height: 100,
+                  padding: const EdgeInsets.all(20),
+                  decoration: BoxDecoration(
+                    color: Theme.of(context)
+                        .colorScheme
+                        .onPrimary
+                        .withOpacity(0.3),
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        const Text("No new event \ninvites :(",
+                            style: TextStyle(
+                                fontSize: 20,
+                                fontWeight: FontWeight.bold)),
+                        SvgPicture.asset(
+                          'assets/images/sad_icon.svg',
+                          height: 75,
+                          fit: BoxFit.contain,
+                        )
+                      ]))
+              : ListView.builder(
+                  itemCount: _invitedEvents.length,
+                  itemBuilder: (context, index) {
+                    return EventInvitation(
+                        inviterId: _invitedEvents[index].creatorID,
+                        event: _invitedEvents[index]);
+                  },
                 ),
-                child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      const Text("No new event \ninvites :(",
-                          style: TextStyle(
-                              fontSize: 20, fontWeight: FontWeight.bold)),
-                      SvgPicture.asset(
-                        'assets/images/sad_icon.svg',
-                        height: 75,
-                        fit: BoxFit.contain,
-                      )
-                    ])),
-          ),
-        )
-        : ListView.builder(
-          itemCount: _invitedEvents.length,
-          itemBuilder: (context, index) {
-            return EventInvitation(
-                inviterId: _invitedEvents[index].creatorID,
-                event: _invitedEvents[index]);
-          },
-        );
+    );
   }
 }
